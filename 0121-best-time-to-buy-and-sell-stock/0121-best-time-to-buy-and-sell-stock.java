69f82f7440ed542dcdaa174f61de5e9694f2ee5d
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-09 18:36:38 +0530

    Time: 2 ms (90.41%), Space: 61.3 MB (25.55%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,18 +1,14 @@
 class Solution {
     public int maxProfit(int[] prices) {
         // find longest increasing slope
-        int minYet=0;
-        int maxYet=minYet;
+        int minYet=prices[0];
         int maxVal=0;
         for(int i=1; i<prices.length; i++) {
-            if(prices[minYet]>prices[i])  {
-                minYet=i;
-                if(i>maxYet) maxYet=i;
-            } else if(prices[i]>prices[maxYet]) {
-                maxYet=i;
-            }
+            if(minYet>prices[i])  {
+                minYet=prices[i];
+            } 
 
-            int curr = prices[maxYet]-prices[minYet];
+            int curr = prices[i]-minYet;
             if(maxVal<curr)
                 maxVal=curr;
 
