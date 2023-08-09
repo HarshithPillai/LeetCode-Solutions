fedd12c23372e324f5bc6465db15dc5a254ae9c6
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-09 17:05:25 +0530

    Time: 0 ms (100%), Space: 41.8 MB (95.89%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,14 +1,26 @@
 class Solution {
-    public void nextPermutation(int[] nums) {
-        // find the first instance of sorted pair and switch them ind1<->ind2
-        // then sort (ind1+1,end of array)
-
+    public void reverse(int[] nums, int ind) {
         int size=nums.length;
+        if(ind>size-1) return;
+        
+        for(int i=ind; i<(ind+size)/2; i++) {
+            if(i<size-i+ind-1 && nums[i]>nums[size-i+ind-1]) {
+                int a=nums[i];
+                nums[i]=nums[size-i+ind-1];
+                nums[size-i+ind-1]=a;
+                
+                //ind=3
+                //a=1
+                //size-i+ind-1 = 4
+                //i=3
+                //nums[i]=3
+            }
+        }
+    }
 
+    public void nextPermutation(int[] nums) {
+        int size=nums.length;
         if(size<2) return;
-
-        boolean swappedFlag = false;
-
         int ind1=-1;
 
         for(int i=size-2; i>=0; i--) {
@@ -18,23 +30,22 @@ class Solution {
             }
         }
         if(ind1>-1) {
-            int a=nums[ind1];
-            int min = -1;
-            int minVal = Integer.MAX_VALUE;
-            for(int i=ind1+1; i<size; i++) {
+            int min = -1, minVal = Integer.MAX_VALUE;
+            
+            for(int i=size-1; i>ind1; i--) {
                 if(nums[ind1]<nums[i] && nums[i]<minVal) {
                     min=i; minVal=nums[i];
                 }
             }
-            // found the min value after the peak that is also greater than nums[ind1]
-            a=nums[ind1];
+            
+            int a=nums[ind1];
             nums[ind1]=nums[min];
             nums[min]=a;
             
-            Arrays.sort(nums, ind1+1, nums.length);
+            reverse(nums, ind1+1);
 
         } else {
-            Arrays.sort(nums);
+            reverse(nums,0);
         }
     }
 }
