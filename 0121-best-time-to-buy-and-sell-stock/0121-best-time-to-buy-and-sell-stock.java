06b0d04ee1f018bc29d0021bf6c4248f3e154767
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-09 18:38:10 +0530

    Time: 2 ms (90.41%), Space: 61.7 MB (6.41%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -4,11 +4,8 @@ class Solution {
         int minYet=prices[0];
         int maxVal=0;
         for(int i=1; i<prices.length; i++) {
-            if(minYet>prices[i])  {
-                minYet=prices[i];
-            } 
-
-            
+            minYet=Math.min(minYet,prices[i]);
+   
             if(maxVal<prices[i]-minYet)
                 maxVal=prices[i]-minYet;
 
