0a5ccca13f17edcdd437ad260d1fcdfe64b7dd8e
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-11 17:59:27 +0530

    Time: 2 ms (98.32%), Space: 47.4 MB (5.37%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -2,9 +2,6 @@ class Solution {
     public List<Integer> majorityElement(int[] nums) {
         List<Integer> res = new ArrayList<Integer>();
         int n=nums.length;
-
-        
-
         int el1=Integer.MIN_VALUE, el2=Integer.MIN_VALUE, count1=0, count2=0;
         for(int i=0; i<n; i++) {
             if(nums[i]==el1 && nums[i]!=el2) {
