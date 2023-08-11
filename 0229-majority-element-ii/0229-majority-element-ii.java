acf762c347dee0b505af4edca5d85de517993aa5
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-11 17:59:10 +0530

    Time: 2 ms (98.32%), Space: 47.3 MB (13.46%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -7,15 +7,15 @@ class Solution {
 
         int el1=Integer.MIN_VALUE, el2=Integer.MIN_VALUE, count1=0, count2=0;
         for(int i=0; i<n; i++) {
-            if(count1==0 && nums[i]!=el2) {
-                count1=1; el1=nums[i];
-            } else if(count2==0 && nums[i]!=el1) {
-                count2=1; el2=nums[i];
-            } else if(nums[i]==el1 && nums[i]!=el2) {
+            if(nums[i]==el1 && nums[i]!=el2) {
                 count1++;
             } else if(nums[i]==el2 && nums[i]!=el1) {
                 count2++;
-            } else {
+            } else if(count1==0 && nums[i]!=el2) {
+                count1=1; el1=nums[i];
+            } else if(count2==0 && nums[i]!=el1) {
+                count2=1; el2=nums[i];
+            }  else {
                 count1--; count2--;
             }
         }
