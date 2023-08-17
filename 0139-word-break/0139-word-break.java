d897bcef54fdf3c59f399c7a7d8803ba9fa69d44
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-17 21:50:06 +0530

    Time: 9 ms (24.24%), Space: 43.5 MB (38.38%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,29 +1,30 @@
 class Solution {
-    public boolean recUtil(List<String> list, String curr, Map<String, Boolean> map) {
+    public boolean recUtil(Set<String> set, String curr, Map<String, Boolean> map) {
         int n=curr.length();
-        int size = list.size();
+        //int size = list.size();
         if(map.containsKey(curr)) return map.get(curr);
         if(n==0) return true;
-        for(int i=0; i<size; i++) {
-            String word = list.get(i);
-            int len = word.length();
-            if(len>n) continue;
-            String a = curr.substring(0, len);
-            String b = curr.substring(len);
-            if(a.equals(word)) {
-                if(recUtil(list, b, map)) {
+        for(int i=0; i<n; i++) {
+            // String word = list.get(i);
+            // int len = word.length();
+            // if(len>n) continue;
+            // String a = curr.substring(0, len);
+            // String b = curr.substring(len);
+            // if(a.equals(word)) {
+            //     if(recUtil(list, b, map)) {
+            //         map.put(curr, true);
+            //         return true;
+            //     }
+            // }
+            String a = curr.substring(0, i+1);
+            String b = curr.substring(i+1);
+            //System.out.println(a+" "+b);
+            if(set.contains(a)) {
+                if(recUtil(set, b, map)) {
                     map.put(curr, true);
                     return true;
                 }
             }
-            // String a = curr.substring(0, i+1);
-            // String b = curr.substring(i+1);
-            // //System.out.println(a+" "+b);
-            // if(set.contains(a)) {
-            //     if(recUtil(set, b)) {
-            //         return true;
-            //     }
-            // }
 
         }
         map.put(curr, false);
@@ -31,11 +32,11 @@ class Solution {
     }
 
     public boolean wordBreak(String s, List<String> wordDict) {
-        // Set<String> set = new HashSet<>();
-        // for(String t:wordDict) {
-        //     set.add(t);
-        // }
+        Set<String> set = new HashSet<>();
+        for(String t:wordDict) {
+            set.add(t);
+        }
         Map<String, Boolean> map = new HashMap<>();
-        return recUtil(wordDict, s, map);
+        return recUtil(set, s, map);
     }
 }
