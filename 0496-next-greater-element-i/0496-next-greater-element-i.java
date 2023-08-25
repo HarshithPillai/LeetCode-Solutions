6e3bbd7811a1b4013427454a89932adca624ba6a
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-25 05:38:37 +0530

    Time: 6 ms (19.94%), Space: 43.3 MB (80.27%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -5,12 +5,14 @@ class Solution {
         for(int i=0; i<n1; i++) {
             int j=nums2.length-1;
             ans[i]=-1;
+            int g=-1;
             while(j>=0 && nums2[j]!=nums1[i]) {
                 if(nums2[j]>nums1[i]) {
-                    ans[i]=nums2[j];
+                    g=nums2[j];
                 }
                 j--;
             }
+            ans[i]=g;
         }
         return ans;
     }
