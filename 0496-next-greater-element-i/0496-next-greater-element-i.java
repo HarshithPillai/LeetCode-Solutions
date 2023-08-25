88f0c446d230c4fa7611b7a02867e9bec605479e
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-25 05:57:55 +0530

    Time: 4 ms (72.39%), Space: 43.5 MB (55.31%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.java
+++ 0496-next-greater-element-i/0496-next-greater-element-i.java
@@ -2,17 +2,30 @@ class Solution {
     public int[] nextGreaterElement(int[] nums1, int[] nums2) {
         int n1=nums1.length;
         int[] ans = new int[n1];
-        for(int i=0; i<n1; i++) {
-            int j=nums2.length-1;
-            ans[i]=-1;
-            int g=-1;
-            while(j>=0 && nums2[j]!=nums1[i]) {
-                if(nums2[j]>nums1[i]) {
-                    g=nums2[j];
-                }
-                j--;
+        //keep a stack and hashmap
+        //iterate through each int in nums2 from end to start
+        //for each element see what is at top of stack
+        // if empty add -1 to that number in map
+        // if top<nums2[i]c, keep on popping till you get a number 
+        // greater than this. or till stack is empty
+        // then add that number to map <nums2[i]=number> and to stack.
+
+        Stack<Integer> stack = new Stack<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i=nums2.length-1; i>=0; i--) {
+            while(!stack.isEmpty() && stack.peek()<=nums2[i]) {
+                stack.pop();
             }
-            ans[i]=g;
+            if(stack.isEmpty()) {
+                map.put(nums2[i], -1);
+            } else {
+                map.put(nums2[i],stack.peek());
+            }
+            stack.push(nums2[i]);
+        }
+        int i=0;
+        for(int num:nums1) {
+            ans[i++] = map.get(num);
         }
         return ans;
     }
