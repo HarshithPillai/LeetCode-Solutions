594ed540c290f4190e2df0a044d231f068364b57
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-26 18:49:56 +0530

    Time: 2 ms (12%), Space: 40.6 MB (65.91%) - LeetHub

---
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void recutil(TreeNode root, List<Integer> preorder, List<Integer> postorder) {
+        if(root==null) {
+            postorder.add(101);
+            preorder.add(101);
+            return;
+        }
+        preorder.add(root.val);
+        recutil(root.left, preorder, postorder);
+        recutil(root.right, preorder, postorder);
+        postorder.add(root.val);
+    }
+    public boolean isSymmetric(TreeNode root) {
+        List<Integer> preorder  = new ArrayList<>();
+        List<Integer> postorder = new ArrayList<>();
+        recutil(root, preorder, postorder);
+        int n=preorder.size();
+        for(int i=0; i<n; i++) {
+            //System.out.println(preorder.get(i)+" "+postorder.get(n-i-1));
+            if(preorder.get(i)!=postorder.get(n-i-1)) return false;
+        }
+        return true;
+    }
+}
