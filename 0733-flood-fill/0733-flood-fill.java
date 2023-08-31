231d432f69a0a1368fe15200db687837be16e48d
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-08-31 18:59:19 +0530

    Time: 0 ms (100%), Space: 44.2 MB (29.2%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,29 +1,28 @@
 class Solution {
-    Set<String> set;
-    // class Pair{
-    //     int sr, sc;
-    //     Pair(int r, int c){
-    //         sr=r; sc=c;
+    // Set<Pairx> set;
+    // class Pairx{
+    //     int row;
+    //     int col;
+    //     Pairx(int r, int c){
+    //         row=r; col=c;
     //     }
     // }
-    public int[][] recutil(int[][] image, int sr, int sc, int color, int initial) {
-        if(set.contains(sr+" "+sc)) return image;
+    public void recutil(int[][] image, int sr, int sc, int color, int initial) {
         int r=image.length;
-        if(sr<0 || sc<0 || sr==r) return image;
-        int c=image[0].length;
-        if(sc==c) return image;
-        set.add(sr+" "+sc);
+        if(sr<0 || sc<0 || sr==r || image[0].length==sc) return;
+        
         if(image[sr][sc]==initial) {
             image[sr][sc]=color;
-            image=recutil(image, sr-1,   sc, color, initial);
-            image=recutil(image,   sr, sc-1, color, initial);
-            image=recutil(image, sr+1,   sc, color, initial);
-            image=recutil(image,   sr, sc+1, color, initial);
+            recutil(image, sr-1,   sc, color, initial);
+            recutil(image,   sr, sc-1, color, initial);
+            recutil(image, sr+1,   sc, color, initial);
+            recutil(image,   sr, sc+1, color, initial);
         }
-        return image;
     }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        set = new HashSet<>();
-        return recutil(image, sr, sc, color, image[sr][sc]);
+        //set = new HashSet<>();
+        if(image[sr][sc]==color) return image;
+        recutil(image, sr, sc, color, image[sr][sc]);
+        return image;
     }
 }
