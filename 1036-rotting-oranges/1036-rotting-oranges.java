48b9fa33b73b624904aaaa8ce5364a45627dae9c
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-03 09:30:58 +0530

    Time: 2 ms (86.49%), Space: 43.7 MB (5.21%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,16 +1,4 @@
 class Solution {
-    // public void recutil(int[][] grid, int i, int j, int time, int m, int n) {
-    //     if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!=1) return;
-        
-    //     grid[i][j]=2;
-    //     time++;
-    //     if(time>maxtime) maxtime=time;
-        
-    //     recutil(grid, i, j-1, time, m, n);
-    //     recutil(grid, i, j+1, time, m, n);
-    //     recutil(grid, i-1, j, time, m, n);
-    //     recutil(grid, i+1, j, time, m, n);
-    // }
     class Pair {
         int i, j;
         Pair(int r, int c) { i=r; j=c; }
