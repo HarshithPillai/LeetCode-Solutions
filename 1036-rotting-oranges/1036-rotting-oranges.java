d0a55994e09f6b1ca0ff3bd67b8bec540119f8e3
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-03 09:25:55 +0530

    Time: 2 ms (86.49%), Space: 43 MB (8.31%) - LeetHub

---
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -0,0 +1,60 @@
+class Solution {
+    // public void recutil(int[][] grid, int i, int j, int time, int m, int n) {
+    //     if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!=1) return;
+        
+    //     grid[i][j]=2;
+    //     time++;
+    //     if(time>maxtime) maxtime=time;
+        
+    //     recutil(grid, i, j-1, time, m, n);
+    //     recutil(grid, i, j+1, time, m, n);
+    //     recutil(grid, i-1, j, time, m, n);
+    //     recutil(grid, i+1, j, time, m, n);
+    // }
+    class Pair {
+        int i, j;
+        Pair(int r, int c) { i=r; j=c; }
+    }
+
+    public int orangesRotting(int[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+
+        Queue<Pair> q = new LinkedList<>();
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j]==2) {
+                    q.add(new Pair(i-1,j));
+                    q.add(new Pair(i+1,j));
+                    q.add(new Pair(i,j-1));
+                    q.add(new Pair(i,j+1));
+                }
+            }
+        }
+        int maxtime=0;
+        while(!q.isEmpty()) {
+            int size=q.size();
+            while(size-->0) {
+                Pair top = q.poll();
+                int i=top.i;
+                int j=top.j;
+                if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!=1) continue;
+                grid[i][j]=2;
+                q.add(new Pair(i-1,j));
+                q.add(new Pair(i+1,j));
+                q.add(new Pair(i,j-1));
+                q.add(new Pair(i,j+1));
+
+            }
+            if(!q.isEmpty()) maxtime++;
+        }
+
+        // check if any orange is still not rotten
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n; j++) {
+                if(grid[i][j]==1) return -1;
+            }
+        }
+        return maxtime;
+    }
+}
