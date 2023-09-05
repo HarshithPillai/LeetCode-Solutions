cf604d34c1ea60ae2068079d95fe8be74e513b1f
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-05 07:57:11 +0530

    Time: 2 ms (86.22%), Space: 41.7 MB (20.52%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,7 +1,7 @@
 class Solution {
     class Pair {
-        int i, j;
-        Pair(int r, int c) { i=r; j=c; }
+        int i, j, step;
+        Pair(int r, int c, int s) { i=r; j=c; step=s; }
     }
 
     public int orangesRotting(int[][] grid) {
@@ -12,29 +12,26 @@ class Solution {
         for(int i=0; i<m; i++) {
             for(int j=0; j<n; j++) {
                 if(grid[i][j]==2) {
-                    q.add(new Pair(i-1,j));
-                    q.add(new Pair(i+1,j));
-                    q.add(new Pair(i,j-1));
-                    q.add(new Pair(i,j+1));
+                    q.add(new Pair(i,j,0));
                 }
             }
         }
         int maxtime=0;
+        int[] dx={1,0,-1,0};
+        int[] dy={0,1,0,-1};
         while(!q.isEmpty()) {
-            int size=q.size();
-            while(size-->0) {
-                Pair top = q.poll();
-                int i=top.i;
-                int j=top.j;
-                if(i<0 || j<0 || i>=m || j>=n || grid[i][j]!=1) continue;
-                grid[i][j]=2;
-                q.add(new Pair(i-1,j));
-                q.add(new Pair(i+1,j));
-                q.add(new Pair(i,j-1));
-                q.add(new Pair(i,j+1));
-
+            Pair top = q.poll();
+            int i=top.i;
+            int j=top.j;
+            int step=top.step;
+            maxtime=step;
+            for(int k=0;k<4;k++) {
+                int r=i+dx[k], c=j+dy[k];
+                if(r>=0 && c>=0 && r<m && c<n && grid[r][c]==1) {
+                    grid[r][c]=2;
+                    q.add(new Pair(r,c,step+1));
+                }
             }
-            if(!q.isEmpty()) maxtime++;
         }
 
         // check if any orange is still not rotten
