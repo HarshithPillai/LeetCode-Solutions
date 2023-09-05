e94aaa7576a6c0eb2934189f9720521bfd4ca343
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-05 14:01:25 +0530

    Time: 4 ms (99.19%), Space: 55 MB (58.45%) - LeetHub

---
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -0,0 +1,34 @@
+class Solution {
+    public boolean helper(int[][] graph, int node, int[] path, int[] visited, int[] check) {
+        
+        path[node]=1;
+        visited[node]=1;
+        for(int num:graph[node]) {
+            if(visited[num]==0) {
+                if(helper(graph, num, path, visited, check)) {
+                    return true;
+                }
+            } else if(path[num]==1) return true;
+        }
+
+        check[node]=1;
+        path[node]=0;
+        return false;
+    }
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+        int n=graph.length;
+        List<Integer> result = new ArrayList<>();
+        int[] path = new int[n];
+        int[] check = new int[n];
+        int[] visited = new int[n];
+        for(int i=0; i<n; i++) {
+            if(visited[i]==0) {
+                helper(graph, i, path, visited, check);
+            }
+        }
+        for(int i=0;i<n;i++) {
+            if(check[i]==1) result.add(i);
+        }
+        return result;
+    }
+}
