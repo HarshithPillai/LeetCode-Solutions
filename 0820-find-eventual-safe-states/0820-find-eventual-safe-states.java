33f91a25977c759b240b500344e8227d8931ba95
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-06 11:43:57 +0530

    Time: 28 ms (22.61%), Space: 55.4 MB (13.15%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,28 +1,34 @@
 class Solution {
-    public boolean helper(int[][] graph, int[] visited, int[] path, int node, int[] check) {
-        visited[node]=1;
-        path[node]=1;
-        for(int num:graph[node]) {
-            if(visited[num]==0) {
-                if(helper(graph, visited, path, num, check)) return true;
-            } else if(path[num]==1) return true;
-        }
-        path[node]=0;
-        check[node]=1;
-        return false;
-    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        int n=graph.length;
-        int[] path = new int[n];
-        int[] check = new int[n];
-        int[] visited = new int[n];
+        List<List<Integer>> revAdj = new ArrayList<>();
         List<Integer> result = new ArrayList<>();
+        int n=graph.length;
+        Queue<Integer> q = new LinkedList<>();
+        int[] indegree = new int[n];
+        
+        for(int i=0; i<n; i++) revAdj.add(new ArrayList<>());
         for(int i=0; i<n; i++) {
-            helper(graph, visited, path, i, check);
+            for(int num:graph[i]) {
+                revAdj.get(num).add(i);
+            }
         }
+
         for(int i=0; i<n; i++) {
-            if(check[i]==1) result.add(i);
+            for(int num:revAdj.get(i)) {
+                indegree[num]++;
+            }
         }
+        for(int i=0; i<n; i++) if(indegree[i]==0) q.add(i);
+
+        while(!q.isEmpty()) {
+            int node=q.poll();
+            result.add(node);
+            for(int num:revAdj.get(node)) {
+                indegree[num]--;
+                if(indegree[num]==0) q.add(num);
+            }
+        }
+        Collections.sort(result);
         return result;
     }
 }
