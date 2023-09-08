f36ec83d86a588f521b515a7eeaa88be73adcb0f
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-08 10:48:50 +0530

    Time: 13 ms (51.08%), Space: 48.5 MB (83.8%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.java
@@ -4,41 +4,42 @@ class Solution {
         Pair(long f, long s) { first=f; second=s; }
     }
     public int countPaths(int n, int[][] roads) {
-        long MOD=(long)(1e9+7);
-        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y)-> (int)(x.first-y.first));
-        //Queue<Pair> pq = new LinkedList<>();
         List<List<Pair>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        for(int i=0;i<roads.length;i++) {
+        for(int i=0; i<n; i++) {
+            adj.add(new ArrayList<>());
+        }
+        for(int i=0; i<roads.length; i++) {
             adj.get(roads[i][0]).add(new Pair(roads[i][1],roads[i][2]));
             adj.get(roads[i][1]).add(new Pair(roads[i][0],roads[i][2]));
         }
+        long MOD = (long)(1e9+7);
         long ways[] = new long[n];
-        long dist[] = new long[n];
-        for(int i=0;i<n;i++) dist[i]=Long.MAX_VALUE;
-        dist[0]=0;
         ways[0]=1;
-        pq.add(new Pair(0,0));//dist, node
+        long dist[] = new long[n];
+        for(int i=0;i<n;i++) {
+            dist[i]=(long)1e18;
+        }
+        dist[0]=0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y)-> (int)(x.first-y.first));
+        pq.add(new Pair(0,0));
         while(!pq.isEmpty()) {
-            Pair top=pq.poll();
+            Pair top = pq.poll();
             long dis = top.first;
             int node = (int)top.second;
-            for(Pair p:adj.get(node)) {
-                int newNode=(int) p.first;
-                long newNodeWeight=p.second;
-                long val=(dis+newNodeWeight);
+            for(Pair nbr:adj.get(node)) {
+                int newNode=(int)nbr.first;
+                long newWeight=nbr.second;
+                long val=dis+newWeight;
                 if(val < dist[newNode]) {
-                    ways[newNode]=ways[node];
-                    dist[newNode]=val;
+                    dist[newNode] = val;
+                    ways[newNode] = ways[node];
                     pq.add(new Pair(val, newNode));
                 } else if(val == dist[newNode]) {
-                    ways[newNode]=(ways[newNode]+ways[node])%MOD;
-                    //pq.add(new Pair((dis+newNodeWeight), newNode));
+                    ways[newNode] = (ways[newNode]+ways[node])%MOD;
+                    //pq.add(new Pair(val, newNode));
                 }
             }
         }
-
-        //if(dist[n-1]==(int)1e9) return -1;
         return (int)(ways[n-1]%MOD);
     }
 }
