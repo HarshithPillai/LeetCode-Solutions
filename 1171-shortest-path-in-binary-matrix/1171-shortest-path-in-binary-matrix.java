5428972329cf5be991dc69600ff382c01e9fa4ce
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-08 11:21:58 +0530

    Time: 28 ms (23.42%), Space: 45.4 MB (14.27%) - LeetHub

--- 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
+++ 1171-shortest-path-in-binary-matrix/1171-shortest-path-in-binary-matrix.java
@@ -5,40 +5,39 @@ class Solution {
     }
     public int shortestPathBinaryMatrix(int[][] grid) {
         if(grid[0][0]==1) return -1;
-        int r=grid.length;
-        int n=r;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y)-> x.dist-y.dist);
         
+        int r=grid.length;
         int c=r;
-        int[][] dist = new int[r][c];
+        int dist[][] = new int[r][c];
         for(int i=0;i<r;i++) {
             for(int j=0;j<c;j++) {
-                dist[i][j]=(int)(1e9);
+                dist[i][j]=(int)1e9;
             }
         }
         dist[0][0]=1;
-        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y)->{
-            return x.dist-y.dist;
-        });
-        pq.add(new Pair(1, 0, 0));
+        pq.add(new Pair(1,0,0));
+
+        int dx[] = {1,0,-1, 0,1,-1,-1,1};
+        int dy[] = {0,1, 0,-1,1,-1, 1,-1};
         while(!pq.isEmpty()) {
             Pair top = pq.poll();
-            int dis=top.dist;
-            int x=top.row;
-            int y=top.col;
-            int[] dx={1,0,-1,0,1,-1,1,-1};
-            int[] dy={0,1,0,-1,1,-1,-1,1};
-            for(int i=0;i<8;i++) {
-                int row=x+dx[i];
-                int col=y+dy[i];
-                if(row>=0 && col>=0 && row<r && col<c && grid[row][col]==0) {
-                    if(dis+1<dist[row][col]) {
-                        dist[row][col]=dis+1;
-                        pq.add(new Pair(dis+1,row,col));
+            int dis = top.dist;
+            int row = top.row;
+            int col = top.col;
+            for(int i=0; i<8; i++) {
+                int newr = row+dx[i];
+                int newc = col+dy[i];
+                if(newr>=0 && newc>=0 && newr<r && newc<c && grid[newr][newc]==0) {
+                    if(dis+1<dist[newr][newc]) {
+                        dist[newr][newc]=dis+1;
+                        pq.add(new Pair(dis+1, newr, newc));
                     }
                 }
             }
         }
-        if(dist[n-1][n-1]==(int)(1e9)) return -1;
-        return dist[n-1][n-1];
+
+        if(dist[r-1][c-1]==(int)1e9) return -1;
+        return dist[r-1][c-1];
     }
 }
