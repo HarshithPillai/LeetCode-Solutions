a6848ab2e8c926bd5ff463a24b1fea02604ed234
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-10 18:03:07 +0530

    Time: 101 ms (53.49%), Space: 102 MB (74.04%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,37 +1,41 @@
 class Solution {
     int timer=1;
-    public void dfs(int node, int[] tim, int[] low, int[] visited, List<List<Integer>> adj, List<List<Integer>> result, int parent) {
-        if(visited[node]==1) return;
+    public void dfs(int node, int[] t_in, int[] low, int[] visited, List<List<Integer>> adj, List<List<Integer>> result, int parent) {
         visited[node]=1;
         low[node]=timer;
-        tim[node]=timer++;
+        t_in[node]=timer++;
         for(int nbr:adj.get(node)) {
             if(nbr==parent) continue;
-            dfs(nbr, tim, low, visited, adj, result, node);
-            if(low[nbr]<low[node]) low[node]=low[nbr];
-            if(tim[node]<low[nbr]) {
-                List<Integer> temp = new ArrayList<>();
-                temp.add(node);temp.add(nbr);
-                result.add(temp);
+            if(visited[nbr]==0) {    
+                dfs(nbr, t_in, low, visited, adj, result, node);
+                low[node] = Math.min(low[nbr], low[node]);
+                if(t_in[node]<low[nbr]) {
+                    List<Integer> temp = new ArrayList<>();
+                    temp.add(node);temp.add(nbr);
+                    result.add(temp);
+                }
+            } else {
+                low[node] = Math.min(low[nbr], low[node]);
             }
         }
     }
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        int tim[] = new int[n];
+        int t_in[] = new int[n];
         int low[] = new int[n];
         int visited[] = new int[n];
 
         List<List<Integer>> result = new ArrayList<>();
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) {
-            low[i] = (int)1e9;
-            adj.add(new ArrayList<>());
-        }
+        
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
+
         for(int i=0;i<connections.size();i++) {
             adj.get(connections.get(i).get(0)).add(connections.get(i).get(1));
             adj.get(connections.get(i).get(1)).add(connections.get(i).get(0));
         }
-        dfs(0, tim, low, visited, adj, result, -1);//////////PARENT -1
+        
+        dfs(0, t_in, low, visited, adj, result, -1);//////////PARENT -1
+        
         return result;
     }
 }
