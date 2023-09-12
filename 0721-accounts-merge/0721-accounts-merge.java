b47874d5ff68fecaa2b77d01df01a723656cf4e0
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-12 15:59:30 +0530

    Time: 27 ms (90.64%), Space: 47 MB (73.25%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,67 +1,68 @@
 class Solution {
     class DisjointSet {
-        List<Integer> size, parent;
+        List<Integer> size=new ArrayList<>(), parent=new ArrayList<>();
         DisjointSet(int n) {
-            size = new ArrayList<>();
-            parent = new ArrayList<>();
             for(int i=0;i<=n;i++) {
                 size.add(1);
                 parent.add(i);
             }
         }
-        public int findUPar(int node) {
-            if(parent.get(node)==node) return node;
-            int ulp = findUPar(parent.get(node));
-            parent.set(node, ulp);
+        public int findUPar(int u) {
+            if(parent.get(u)==u) return u;
+            int ulp = findUPar(parent.get(u));
+            parent.set(u, ulp);
             return ulp;
         }
         public void unionBySize(int u, int v) {
             int ulp_u = findUPar(u);
             int ulp_v = findUPar(v);
-            int sizeU = size.get(ulp_u);
-            int sizeV = size.get(ulp_v);
-            if(ulp_u == ulp_v) return;
-            if(sizeU < sizeV) {
+            if(ulp_v == ulp_u) return;
+            if(size.get(ulp_u)<size.get(ulp_v)) {
+                size.set(ulp_v, size.get(ulp_u)+size.get(ulp_v));
                 parent.set(ulp_u, ulp_v);
             } else {
                 parent.set(ulp_v, ulp_u);
+                size.set(ulp_u, size.get(ulp_u)+size.get(ulp_v));
             }
-            size.set(ulp_u, sizeU+sizeV);
         }
     }
-    public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        int V=accounts.size();
+    public List<List<String>> accountsMerge(List<List<String>> adj) {
+        int n=adj.size();
+        DisjointSet ds = new DisjointSet(n);
+        // different rows with some common ids between them
+        // merge all the rows who have a single email id in common
+        // create a map<String, Int> key is email and value is the row it was in
+        // if an email is found that is not in map, add it with the row number
+        // else if found in map, then add then union the current row with the row in the map
+        
         Map<String, Integer> map = new HashMap<>();
-        DisjointSet ds = new DisjointSet(V);
-        for(int i=0;i<V;i++) {
-            int col=accounts.get(i).size();
-            for(int j=1;j<col;j++) {
-                String email=accounts.get(i).get(j);
-                if(map.containsKey(email)) {
-                    ds.unionBySize(i, map.get(email));
+        for(int i=0;i<n;i++) {
+            for(int j=1;j<adj.get(i).size();j++) {
+                String str=adj.get(i).get(j);
+                if(map.containsKey(str)) {
+                    ds.unionBySize(i, map.get(str));
                 } else {
-                    map.put(email,i);
+                    map.put(str, i);
                 }
             }
         }
-        List<List<String>> mergeList = new ArrayList<>();
-        for(int i=0;i<V;i++) {
-            mergeList.add(new ArrayList<>());
-        }
+        List<List<String>> temp = new ArrayList<>();
+        for(int i=0;i<n;i++) temp.add(new ArrayList<>());
         for(Map.Entry<String, Integer> entry:map.entrySet()) {
-            int index=ds.findUPar(entry.getValue());
-            String val=entry.getKey();
-            mergeList.get(index).add(val);
+            String str = entry.getKey();
+            int row = entry.getValue();
+            int ulp = ds.findUPar(row);
+            temp.get(ulp).add(str);
         }
-        
         List<List<String>> result = new ArrayList<>();
-        for(int i=0;i<V;i++) {
-            if(mergeList.get(i).size()==0) continue;
-            Collections.sort(mergeList.get(i));
-            List<String> temp = new ArrayList<>();
-            temp.add(accounts.get(i).get(0));
-            for(String ele:mergeList.get(i)) temp.add(ele);
-            result.add(temp);
+        for(int i=0;i<n;i++) {
+            if(temp.get(i).size()>0) {
+                List<String> t = new ArrayList<>();
+                Collections.sort(temp.get(i));
+                t.add(adj.get(i).get(0));
+                for(String str:temp.get(i)) t.add(str);
+                result.add(t);
+            }
         }
         return result;
     }
