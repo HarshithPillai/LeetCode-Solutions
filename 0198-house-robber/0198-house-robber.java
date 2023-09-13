0c0626de8c72b9d18318d431255ed29be1349805
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-13 13:06:44 +0530

    Time: 0 ms (100%), Space: 39.9 MB (53.02%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,14 @@
 class Solution {
-    public int recutil(int[] nums, int ind, int[] dp) {
-        if(ind==0) return nums[ind];
-        if(ind<0) return 0;
-        if(dp[ind]>-1) return dp[ind];
-        int pick = nums[ind]+recutil(nums, ind-2,dp);
-        int notPicked = recutil(nums, ind-1,dp);
-        dp[ind]=Math.max(pick, notPicked);
-        return dp[ind];
-    }
     public int rob(int[] nums) {
         int n=nums.length;
+        if(n==1) return nums[0];
         int[] dp = new int[n];
         Arrays.fill(dp, -1);
-        
-        return recutil(nums, n-1, dp);
+        dp[0]=nums[0];
+        dp[1]=Math.max(dp[0], nums[1]);
+        for(int i=2;i<n;i++) {
+            dp[i]=Math.max(nums[i]+dp[i-2], dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
