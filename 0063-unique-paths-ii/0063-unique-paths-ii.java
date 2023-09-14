608ad973e67fa71490a4432a4bed243cfd6b44df
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-14 11:16:26 +0530

    Time: 0 ms (100%), Space: 40.1 MB (97.1%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.java
+++ 0063-unique-paths-ii/0063-unique-paths-ii.java
@@ -4,18 +4,18 @@ class Solution {
         if(grid[0][0]==1 || grid[r-1][c-1]==1) return 0;
         if(r==1 && c==1) return 1;
 
-        int[][] dp = new int[r][c];
-        dp[0][0] = 1;
+        int[] dp = new int[c];
+        dp[0] = 1;
 
         for(int i=0;i<r;i++) {
             for(int j=0;j<c;j++) {
                 if(i==0 && j==0) continue;
                 if(grid[i][j]==0) {
-                    dp[i][j] = ((i>0)?(dp[i-1][j]):0) + ((j>0)?dp[i][j-1]:0);
-                }
+                    dp[j] = ((i>0)?(dp[j]):0) + ((j>0)?dp[j-1]:0);
+                } else dp[j]=0;
             }
         }
 
-        return dp[r-1][c-1];
+        return dp[c-1];
     }
 }
