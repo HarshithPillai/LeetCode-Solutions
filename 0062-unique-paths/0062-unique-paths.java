6159e4b5bc149aa1489a1b832bb0c9414a0c2d75
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-14 10:14:30 +0530

    Time: 0 ms (100%), Space: 38.7 MB (99.49%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,15 +1,14 @@
 class Solution {
+    public int recutil(int i, int j, int m, int n, int[][] dp) {
+        if(i<0 || j<0 || i==m || j==n) return 0;
+        if(i==m-1 || j==n-1) return 1;
+        if(dp[i][j]>-1) return dp[i][j];
+        dp[i][j] = recutil(i+1, j, m, n, dp) + recutil(i, j+1, m, n, dp);
+        return dp[i][j];
+    }
     public int uniquePaths(int m, int n) {
-        int[][] dp = new int[m][n];
-
-        for(int i=0; i<m; i++) {
-            for(int j=0; j<n; j++) {
-                if(i==0 || j==0) dp[i][j]=1;
-                else {
-                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
-                }
-            }
-        }
-        return dp[m-1][n-1];
+        int dp[][] = new int[m][n];
+        for(int[] i:dp) Arrays.fill(i, -1);
+        return recutil(0,0,m,n,dp);
     }
 }
