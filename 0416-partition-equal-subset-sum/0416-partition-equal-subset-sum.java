c55857f1a4cdbfdee3c773390b379d8d6f856c5e
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-15 09:52:42 +0530

    Time: 46 ms (51.42%), Space: 46.8 MB (47.31%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -17,8 +17,18 @@ class Solution {
         if(sum%2==1) return false;
         sum/=2;
         int n=nums.length;
-        int dp[][] = new int[n][sum+1];
-        for(int[] i:dp) Arrays.fill(i, -1);
-        return rec(nums, sum, 0, dp, n);
+        boolean dp[][] = new boolean[n+1][sum+1];
+        
+        for(int i=0;i<=n;i++) {
+            dp[i][0]=true;
+        }
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=sum;j++) {
+                dp[i][j]=dp[i-1][j];
+                if(dp[i-1][j]==false && j-nums[i-1]>=0 && dp[i-1][j-nums[i-1]])
+                    dp[i][j]=true;
+            }
+        }
+        return dp[n][sum];
     }
 }
