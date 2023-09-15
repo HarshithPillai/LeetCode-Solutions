e7d7337a53c2aedc07933328c1b618114bbb115d
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-15 09:40:30 +0530

    Time: 92 ms (22.36%), Space: 55.9 MB (27.65%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,11 +1,11 @@
 class Solution {
-    public boolean rec(int[] nums, int target, int ind, int[][] dp) {
+    public boolean rec(int[] nums, int target, int ind, int[][] dp, int n) {
         if(target<0) return false;
         if(target==0) return true;
-        if(ind==nums.length) return false;
+        if(ind==n) return false;
         if(dp[ind][target]!=-1) return dp[ind][target]==1;
-        boolean take = rec(nums, target-nums[ind], ind+1, dp);
-        boolean dont = rec(nums, target, ind+1, dp);
+        boolean take = rec(nums, target-nums[ind], ind+1, dp,n);
+        boolean dont = rec(nums, target, ind+1, dp,n);
         if(take||dont) {
             dp[ind][target]=1;
         } else dp[ind][target]=0;
@@ -15,8 +15,10 @@ class Solution {
         int sum = 0;
         for(int i: nums) sum+=i;
         if(sum%2==1) return false;
-        int dp[][] = new int[nums.length][sum/2+1];
+        sum/=2;
+        int n=nums.length;
+        int dp[][] = new int[n][sum+1];
         for(int[] i:dp) Arrays.fill(i, -1);
-        return rec(nums, sum/2, 0, dp);
+        return rec(nums, sum, 0, dp, n);
     }
 }
