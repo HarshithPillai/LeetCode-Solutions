661b790e33ae19675af82ae92c553399b348a5c0
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-16 11:23:48 +0530

    Time: 5 ms (86.85%), Space: 42.8 MB (52.77%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -5,6 +5,16 @@ class Solution {
         int sum = 0;
         for(int i:nums) sum+=i;
         if(sum<target) return 0;
+        /**
+            divide into 2 partitions S1 and S2
+            where sum of S1 and S2 :
+            s2 + s1 = total sum
+            s2 = totalSum - s1    - (i)
+            and since s1>s2:
+            s2 = s1 - target      -(ii)
+            add eqn. (i) and (ii):
+            2*s2 = sum - target
+        */
         if((sum-target)%2==1) return 0;
         sum = (sum-target)/2;
         int[][] dp = new int[n+1][sum+1];
