fb18c028fffb7ebf72bf16cd13ec60fcd24c5c8b
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-16 10:50:14 +0530

    Time: 12 ms (58.65%), Space: 43.3 MB (39.02%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,20 +1,21 @@
 class Solution {
-    public int helper(int[] nums, int target, int ind, int[][] dp) {
+    int[][] dp;
+    public int helper(int[] nums, int target, int ind) {
         if(ind==-1) {
             if(target==0) return 1;
             return 0;
         }
         //if(target<0) return 0;
         if(dp[ind][target+2000]!=-1) return dp[ind][target+2000];
-        int add = helper(nums, target-nums[ind], ind-1, dp);
-        int sub = helper(nums, target+nums[ind], ind-1, dp);
+        int add = helper(nums, target-nums[ind], ind-1);
+        int sub = helper(nums, target+nums[ind], ind-1);
         dp[ind][target+2000] = add+sub;
         return add+sub;
     }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
-        int[][] dp = new int[n+1][4001];
+        dp = new int[n+1][4001];
         for(int[] i:dp) Arrays.fill(i,-1);
-        return helper(nums, target, nums.length-1,dp);
+        return helper(nums, target, nums.length-1);
     }
 }
