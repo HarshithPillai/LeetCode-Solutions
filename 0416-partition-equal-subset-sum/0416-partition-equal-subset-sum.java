366499d605f2f13d984b358959a9914e40c0aae3
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-17 11:42:52 +0530

    Time: 41 ms (59.22%), Space: 46.7 MB (49.4%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,32 +1,17 @@
 class Solution {
-    public boolean rec(int[] nums, int target, int ind, int[][] dp, int n) {
-        if(target<0) return false;
-        if(target==0) return true;
-        if(ind==n) return false;
-        if(dp[ind][target]!=-1) return dp[ind][target]==1;
-        boolean take = rec(nums, target-nums[ind], ind+1, dp,n);
-        boolean dont = rec(nums, target, ind+1, dp,n);
-        if(take||dont) {
-            dp[ind][target]=1;
-        } else dp[ind][target]=0;
-        return take||dont;
-    }
     public boolean canPartition(int[] nums) {
-        int sum = 0;
-        for(int i: nums) sum+=i;
+        int sum=0, n=nums.length;
+        for(int i:nums) sum+=i;
         if(sum%2==1) return false;
         sum/=2;
-        int n=nums.length;
-        boolean dp[][] = new boolean[n+1][sum+1];
-        
-        for(int i=0;i<=n;i++) {
-            dp[i][0]=true;
-        }
+        boolean[][] dp = new boolean[n+1][sum+1];
+        dp[0][0]=true;
         for(int i=1;i<=n;i++) {
-            for(int j=1;j<=sum;j++) {
-                dp[i][j]=dp[i-1][j];
-                if(dp[i-1][j]==false && j-nums[i-1]>=0 && dp[i-1][j-nums[i-1]])
-                    dp[i][j]=true;
+            for(int j=0;j<=sum;j++) {
+                boolean dont = dp[i-1][j];
+                boolean pick = false;
+                if(j-nums[i-1]>=0) pick = dp[i-1][j-nums[i-1]];
+                dp[i][j]=pick || dont;
             }
         }
         return dp[n][sum];
