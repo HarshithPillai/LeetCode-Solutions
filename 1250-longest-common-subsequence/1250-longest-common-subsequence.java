b1d80fad658c1f39a9e3b9b46f8eb758dcc00c52
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-17 16:04:08 +0530

    Time: 13 ms (97.15%), Space: 48.4 MB (34.36%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.java
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.java
@@ -1,30 +1,16 @@
 class Solution {
-    public int helper(String s1, String s2, int i1, int i2, int[][] dp) {
-        if(i1<0 || i2<0) return 0;
-        if(dp[i1][i2]!=-1) return dp[i1][i2];
-        int res = 0;
-        if(s1.charAt(i1)==s2.charAt(i2)) {
-            res = 1 + helper(s1,s2,i1-1,i2-1,dp);
-        }
-        int first = helper(s1,s2,i1-1,i2,dp);
-        int second = helper(s1,s2,i1,i2-1,dp);
-        res = Math.max(res, Math.max(first, second));
-        dp[i1][i2] = res;
-        return res;
-    }
     public int longestCommonSubsequence(String text1, String text2) {
-        int n1=text1.length(), n2=text2.length();
-        int[][] dp = new int[n1+1][n2+1];
-        //for(int[] i:dp) Arrays.fill(i,-1);
-        for(int i=1;i<=n1;i++) {
-            for(int j=1;j<=n2;j++) {
-                if(text1.charAt(i-1)==text2.charAt(j-1)) {
+        char[] sarr = text1.toCharArray(), tarr=text2.toCharArray();
+        int s=sarr.length, t=tarr.length;
+        int[][] dp = new int[s+1][t+1];
+        for(int i=1;i<=s;i++) {
+            for(int j=1;j<=t;j++) {
+                if(sarr[i-1]==tarr[j-1]) {
                     dp[i][j]=1+dp[i-1][j-1];
-                } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
+                dp[i][j]=Math.max(dp[i][j], Math.max(dp[i-1][j],dp[i][j-1]));
             }
         }
-        return dp[n1][n2];
+        return dp[s][t];
     }
 }
