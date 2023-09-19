0962d1e6ec91310c66a6750b3e7c508f3343babf
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-19 10:06:38 +0530

    Time: 14 ms (81.53%), Space: 44.5 MB (88.33%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,6 +1,6 @@
 class Solution {
     public String shortestCommonSupersequence(String a, String b) {
-                int m=a.length(), n=b.length();
+        int m=a.length(), n=b.length();
         int[][] dp = new int[m+1][n+1];
         for(int i=1;i<=m;i++) {
             for(int j=1;j<=n;j++) {
@@ -17,29 +17,30 @@ class Solution {
         int extraInd = len-dp[m][n]-1;
         int ind=dp[m][n]-1;
         int x=m,y=n;
-        String ans="";
+        
+        StringBuilder input1 = new StringBuilder();
         while(x>0 && y>0) {
             if(a.charAt(x-1)==b.charAt(y-1)) {
-                ans+=a.charAt(x-1);
+                input1.append(a.charAt(x-1));
                 x--;y--;
             } else if(dp[x-1][y] > dp[x][y-1]) {
-                ans+=a.charAt(x-1); x--;
+                input1.append(a.charAt(x-1)); x--;
             } else {
-                ans+=b.charAt(y-1); y--;
+                input1.append(b.charAt(y-1)); y--;
             }
         }
         while(x>0) {
-            ans+=a.charAt(x-1);x--;
+            input1.append(a.charAt(x-1));x--;
         }
         while(y>0) {
-            ans+=b.charAt(y-1);y--;
+            input1.append(b.charAt(y-1));y--;
         }
         
  
-        StringBuilder input1 = new StringBuilder();
+        
  
         // append a string into StringBuilder input1
-        input1.append(ans);
+        
  
         // reverse StringBuilder input1
         return input1.reverse().toString();
