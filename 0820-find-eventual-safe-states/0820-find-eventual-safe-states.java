973f69277826d483132a6b526695207d093c1f42
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-19 09:12:09 +0530

    Time: 25 ms (33.1%), Space: 55.1 MB (35.86%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -1,34 +1,35 @@
 class Solution {
+    /* 
+        approach: using Kahn's algo
+        reveres all the edges of the graph and get all the new indegrees for each nodes
+        and on each occurence of the node in the bfs traversal, just keep reducing their respective indegrees
+        when a node's indegree drops to 0 add it to queue.
+    */
     public List<Integer> eventualSafeNodes(int[][] graph) {
         List<List<Integer>> revAdj = new ArrayList<>();
-        List<Integer> result = new ArrayList<>();
         int n=graph.length;
-        Queue<Integer> q = new LinkedList<>();
         int[] indegree = new int[n];
-        
-        for(int i=0; i<n; i++) revAdj.add(new ArrayList<>());
-        for(int i=0; i<n; i++) {
-            for(int num:graph[i]) {
-                revAdj.get(num).add(i);
+        for(int i=0;i<n;i++) revAdj.add(new ArrayList<>());
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<graph[i].length;j++) {
+                revAdj.get(graph[i][j]).add(i);
+                indegree[i]++;
             }
         }
-
-        for(int i=0; i<n; i++) {
-            for(int num:revAdj.get(i)) {
-                indegree[num]++;
-            }
-        }
-        for(int i=0; i<n; i++) if(indegree[i]==0) q.add(i);
-
+        Queue<Integer> q = new LinkedList<>();
+        for(int i=0;i<n;i++) if(indegree[i]==0) q.add(i);
+        List<Integer> res = new ArrayList<>();
         while(!q.isEmpty()) {
-            int node=q.poll();
-            result.add(node);
-            for(int num:revAdj.get(node)) {
-                indegree[num]--;
-                if(indegree[num]==0) q.add(num);
+            int node = q.poll();
+            res.add(node);
+            for(int nbr:revAdj.get(node)) {
+                indegree[nbr]--;
+                if(indegree[nbr]==0) {
+                    q.add(nbr);
+                }
             }
         }
-        Collections.sort(result);
-        return result;
+        Collections.sort(res);
+        return res;
     }
 }
