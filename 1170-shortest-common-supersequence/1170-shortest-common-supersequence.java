23064ad4ddff63c1c840ec17ff8c4ef1539e6d74
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-19 10:09:15 +0530

    Time: 7 ms (100%), Space: 45 MB (50.76%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,10 +1,11 @@
 class Solution {
-    public String shortestCommonSupersequence(String a, String b) {
-        int m=a.length(), n=b.length();
+    public String shortestCommonSupersequence(String a1, String b1) {
+        int m=a1.length(), n=b1.length();
+        char[] a=a1.toCharArray(), b=b1.toCharArray();
         int[][] dp = new int[m+1][n+1];
         for(int i=1;i<=m;i++) {
             for(int j=1;j<=n;j++) {
-                if(a.charAt(i-1)==b.charAt(j-1))
+                if(a[i-1]==b[j-1])
                     dp[i][j]=1+dp[i-1][j-1];
                 else {
                     dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
@@ -20,29 +21,22 @@ class Solution {
         
         StringBuilder input1 = new StringBuilder();
         while(x>0 && y>0) {
-            if(a.charAt(x-1)==b.charAt(y-1)) {
-                input1.append(a.charAt(x-1));
+            if(a[x-1]==b[y-1]) {
+                input1.append(a[x-1]);
                 x--;y--;
             } else if(dp[x-1][y] > dp[x][y-1]) {
-                input1.append(a.charAt(x-1)); x--;
+                input1.append(a[x-1]); x--;
             } else {
-                input1.append(b.charAt(y-1)); y--;
+                input1.append(b[y-1]); y--;
             }
         }
         while(x>0) {
-            input1.append(a.charAt(x-1));x--;
+            input1.append(a[x-1]);x--;
         }
         while(y>0) {
-            input1.append(b.charAt(y-1));y--;
+            input1.append(b[y-1]);y--;
         }
-        
- 
-        
- 
-        // append a string into StringBuilder input1
-        
- 
-        // reverse StringBuilder input1
+
         return input1.reverse().toString();
     }
 }
