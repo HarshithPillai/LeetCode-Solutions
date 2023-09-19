a4a40eab99a8c4aacceb1772282a7af13f1ae9ac
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-19 09:27:20 +0530

    Time: 4 ms (98.62%), Space: 54.9 MB (58.34%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.java
@@ -5,31 +5,26 @@ class Solution {
         and on each occurence of the node in the bfs traversal, just keep reducing their respective indegrees
         when a node's indegree drops to 0 add it to queue.
     */
+    public boolean helper(int[][] graph, int[] vis, int[] check, int[] path, int node) {
+        vis[node]=1;
+        path[node]=1;
+        for(int nbr:graph[node]) {
+            if(vis[nbr]==0) {
+                if(helper(graph, vis, check, path, nbr)) return true;
+            } else if(path[nbr]==1) return true;
+        }
+        path[node]=0;
+        check[node]=1;
+        return false;
+    }
     public List<Integer> eventualSafeNodes(int[][] graph) {
-        List<List<Integer>> revAdj = new ArrayList<>();
         int n=graph.length;
-        int[] indegree = new int[n];
-        for(int i=0;i<n;i++) revAdj.add(new ArrayList<>());
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<graph[i].length;j++) {
-                revAdj.get(graph[i][j]).add(i);
-                indegree[i]++;
-            }
-        }
-        Queue<Integer> q = new LinkedList<>();
-        for(int i=0;i<n;i++) if(indegree[i]==0) q.add(i);
+        int[] check = new int[n];
+        int[] vis   = new int[n];
+        int[] path  = new int[n];
+        for(int i=0;i<n;i++)if(vis[i]==0)helper(graph, vis, check, path, i);
         List<Integer> res = new ArrayList<>();
-        while(!q.isEmpty()) {
-            int node = q.poll();
-            res.add(node);
-            for(int nbr:revAdj.get(node)) {
-                indegree[nbr]--;
-                if(indegree[nbr]==0) {
-                    q.add(nbr);
-                }
-            }
-        }
-        Collections.sort(res);
+        for(int i=0;i<n;i++) if(check[i]==1) res.add(i);
         return res;
     }
 }
