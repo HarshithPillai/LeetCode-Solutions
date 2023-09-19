389007f64ae5bbbc7030ef34234803e9f648f940
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-19 20:56:53 +0530

    Time: 16 ms (80.94%), Space: 46.4 MB (40.26%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.java
+++ 0044-wildcard-matching/0044-wildcard-matching.java
@@ -1,27 +1,25 @@
 class Solution {
-    public int helper(char[] pat, char[] str, int i, int j, int[][] dp) {
+    public int helper(char[] a, char[] b, int i, int j,int[][] dp) {
         if(i==-1 && j==-1) return 1;
-        if(j==-1) {
-            while(i>=0 && pat[i]=='*') i--;
-            if(i==-1) return 1;
-            else return 0;
+        if(i==-1) {
+            while(j>=0 && b[j]=='*') j--;
+            if(j==-1) return 1;
+            return 0;
         }
-        if(i==-1) return 0;
+        if(j==-1) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(pat[i]==str[j] || pat[i]=='?') {
-            return dp[i][j] = helper(pat,str,i-1,j-1,dp);
-        }
-        if(pat[i]=='*') {
-            return dp[i][j] = Math.max(helper(pat,str,i-1,j,dp),helper(pat,str,i,j-1,dp));
+        if(a[i]==b[j] || b[j]=='?') {
+            return dp[i][j] = helper(a,b,i-1,j-1,dp);
+        } else if(b[j]=='*') {
+            return dp[i][j] = Math.max(helper(a,b,i-1,j,dp),helper(a,b,i,j-1,dp));
         }
         return dp[i][j]=0;
     }
-    public boolean isMatch(String str, String pattern) {
-        int n1=pattern.length(), n2=str.length();
-        char[] pat=pattern.toCharArray(), s=str.toCharArray();
+    public boolean isMatch(String s, String p) {
+        char[] sarr=s.toCharArray(), tarr=p.toCharArray();
+        int n1=sarr.length,n2=tarr.length;
         int[][] dp = new int[n1+1][n2+1];
         for(int[] i:dp) Arrays.fill(i,-1);
-        
-        return (helper(pat, s, n1-1, n2-1,dp)==1);
+        return (helper(sarr,tarr,n1-1,n2-1,dp)==1);
     }
 }
