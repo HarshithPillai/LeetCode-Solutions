a90f06e004f3953905199f98e6221fab3193e68a
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-19 11:45:38 +0530

    Time: 3 ms (96.56%), Space: 43.2 MB (93.44%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,7 +1,6 @@
 class Solution {
     public int helper(char[] s, char[] t, int i, int j, int[][] dp) {
         // insert
-        if(i==-1 && j==-1) return 0;
         if(j==-1) return i+1;
         if(i==-1) return j+1;
         int take = Integer.MAX_VALUE;
@@ -15,11 +14,11 @@ class Solution {
         return res;
     }
     public int minDistance(String word1, String word2) {
-        if(word1.equals(word2)) return 0;
+        
         char[] s=word1.toCharArray(), t=word2.toCharArray();
         
         int n1=s.length,n2=t.length;
-        if(n1==0) return n2;
+        
         int[][] dp = new int[n1+1][n2+1];
         for(int[] i:dp) Arrays.fill(i,-1);
         return helper(s,t,n1-1,n2-1,dp);
