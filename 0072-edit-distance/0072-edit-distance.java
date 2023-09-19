ad8a0fb394e9d445937d58b5bc0a9c0111a91819
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-19 20:35:13 +0530

    Time: 4 ms (79.6%), Space: 43.3 MB (87.1%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,26 +1,22 @@
 class Solution {
-    public int helper(char[] s, char[] t, int i, int j, int[][] dp) {
-        // insert
-        if(j==-1) return i+1;
+    public int helper(char[] sarr, char[] tarr, int i, int j,int[][] dp) {
         if(i==-1) return j+1;
-        int take = Integer.MAX_VALUE;
+        if(j==-1) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(s[i]==t[j]) return dp[i][j]=helper(s,t,i-1,j-1,dp);
-        int insert = helper(s,t,i,j-1,dp);
-        int delete = helper(s,t,i-1,j,dp);
-        int replace = helper(s,t,i-1,j-1,dp);
-        int res = 1 + Math.min(Math.min(replace,delete),insert);
-        dp[i][j]=res;
-        return res;
+        if(sarr[i]==tarr[j]) {
+            return helper(sarr,tarr,i-1,j-1,dp);
+        }
+        int insert = 1 + helper(sarr,tarr,i,j-1,dp);
+        int delete = 1 + helper(sarr,tarr,i-1,j,dp);
+        int replace = 1+ helper(sarr,tarr,i-1,j-1,dp);
+        int res = Math.min(insert,Math.min(delete,replace));
+        return dp[i][j] = res;
     }
     public int minDistance(String word1, String word2) {
-        
-        char[] s=word1.toCharArray(), t=word2.toCharArray();
-        
-        int n1=s.length,n2=t.length;
-        
+        char[] sarr=word1.toCharArray(), tarr=word2.toCharArray();
+        int n1=sarr.length,n2=tarr.length;
         int[][] dp = new int[n1+1][n2+1];
         for(int[] i:dp) Arrays.fill(i,-1);
-        return helper(s,t,n1-1,n2-1,dp);
+        return helper(sarr,tarr,n1-1,n2-1,dp);
     }
 }
