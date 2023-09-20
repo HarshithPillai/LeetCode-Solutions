eb742f9f356df964e1b3938d0e276ad5fbb39b38
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-20 10:19:02 +0530

    Time: 56 ms (34.9%), Space: 42.9 MB (86.62%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -28,16 +28,16 @@ class Solution {
         // return dp[0];
         Arrays.fill(dp,1);
         // each element in itself is an inceasing subsequence
-        int max=-1;
+        int max=1;
         for(int i=0;i<n;i++) {
             for(int j=0;j<i;j++) {
                 if(nums[i]>nums[j] && dp[i]<dp[j]+1) {
                     dp[i]=dp[j]+1;
-                    max=Math.max(max,dp[i]);
                 }
+                max=Math.max(max,dp[i]);
             }
         }
-        if(max==-1) return 1;
+        
         return max;
     }
 }
