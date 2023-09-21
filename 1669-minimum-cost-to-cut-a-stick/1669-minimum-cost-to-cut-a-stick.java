9692d0415be2f9217a6c265ee2d000ad6e18a660
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-21 14:24:59 +0530

    Time: 11 ms (87.36%), Space: 41.3 MB (53.7%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -17,13 +17,27 @@ class Solution {
         // other wise we might get a case [1,2,6] [3,5]. 
         // here the entire 2nd array is within the range of first array
         int size=cuts.length;
-        dp=new int[size+1][size+1];
-        for(int i[]:dp) Arrays.fill(i,-1);
+        dp=new int[size+2][size+2];
+        
         int[] newcuts= new int[size+2];
         for(int i=1;i<=size;i++) {
             newcuts[i]=cuts[i-1];
         }
         newcuts[size+1]=n;
-        return helper(newcuts,1,size);
+
+        for(int i=size; i>0; i--) {
+            for(int j=i;j<=size;j++) {
+                int res=Integer.MAX_VALUE;
+                for(int k=i;k<=j;k++) {
+                    int current = newcuts[j+1] - newcuts[i-1];
+                    int first   = dp[i][k-1];
+                    int second  = dp[k+1][j];
+                    res = Math.min(res,current + first + second);
+                }
+                dp[i][j]=res;
+            }
+        }
+
+        return dp[1][size];
     }
 }
