ce2677c0d908f658a881bfff67a23e2615b2698a
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-22 10:33:54 +0530

    Time: 5 ms (99.07%), Space: 41 MB (45.86%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -11,9 +11,20 @@ class Solution {
         return dp[s]=res;
     }
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int n=arr.length;
+        int n=arr.length,e=n-1;
         dp = new int[n+1];
-        Arrays.fill(dp,-1);
-        return helper(arr,0,n-1,k);
+        // Arrays.fill(dp,-1);
+        for(int s=n-1;s>=0;s--) {
+            int res=0, max=0;
+            for(int i=s;i<=e && (i-s+1)<=k ;i++) {
+                if(max<arr[i]) max=arr[i];
+                res=Math.max(res, max*(i-s+1) + dp[i+1]);
+            }
+            dp[s]=res;
+        }
+        return dp[0];
     }
 }
+// TC: recursive: N^N
+// with memoization: N^2
+// SC: N for dp array + N for recursive stack space
