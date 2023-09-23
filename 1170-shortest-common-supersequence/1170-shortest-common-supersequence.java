17282c239ed73cd16e7ecc3329875d8f7d6b1e59
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-23 15:25:11 +0530

    Time: 8 ms (99.2%), Space: 44.4 MB (93.11%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,42 +1,41 @@
 class Solution {
-    public String shortestCommonSupersequence(String a1, String b1) {
-        int m=a1.length(), n=b1.length();
-        char[] a=a1.toCharArray(), b=b1.toCharArray();
-        int[][] dp = new int[m+1][n+1];
-        for(int i=1;i<=m;i++) {
-            for(int j=1;j<=n;j++) {
-                if(a[i-1]==b[j-1])
+    public String shortestCommonSupersequence(String str1, String str2) {
+        StringBuilder sb = new StringBuilder();
+        char[] a=str1.toCharArray(),b=str2.toCharArray();
+        int n1=a.length, n2=b.length;
+        int[][] dp = new int[n1+1][n2+1];
+        for(int i=1;i<=n1;i++) {
+            for(int j=1;j<=n2;j++) {
+                if(a[i-1]==b[j-1]) {
                     dp[i][j]=1+dp[i-1][j-1];
-                else {
-                    dp[i][j]=Math.max(dp[i][j-1],dp[i-1][j]);
+                } else {
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        int len = m+n-(dp[m][n]);
-        char[] lcs = new char[dp[m][n]];
-        char[] extra = new char[len-dp[m][n]];
-        int extraInd = len-dp[m][n]-1;
-        int ind=dp[m][n]-1;
-        int x=m,y=n;
-        
-        StringBuilder input1 = new StringBuilder();
+        int x=n1, y=n2;
         while(x>0 && y>0) {
             if(a[x-1]==b[y-1]) {
-                input1.append(a[x-1]);
+                sb.append(a[x-1]);
                 x--;y--;
-            } else if(dp[x-1][y] > dp[x][y-1]) {
-                input1.append(a[x-1]); x--;
             } else {
-                input1.append(b[y-1]); y--;
+                if(dp[x][y-1]>dp[x-1][y]) {
+                    sb.append(b[y-1]);
+                    y--;
+                } else {
+                    sb.append(a[x-1]);
+                    x--;
+                }
             }
         }
         while(x>0) {
-            input1.append(a[x-1]);x--;
+            sb.append(a[x-1]);
+            x--;
         }
         while(y>0) {
-            input1.append(b[y-1]);y--;
+            sb.append(b[y-1]);
+            y--;
         }
-
-        return input1.reverse().toString();
+        return sb.reverse().toString();
     }
 }
