0850ef81c1ed5e30c4e669d018610def052d3720
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-23 16:47:44 +0530

    Time: 31 ms (19.6%), Space: 64.4 MB (12.23%) - LeetHub

--- 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
+++ 0714-best-time-to-buy-and-sell-stock-with-transaction-fee/0714-best-time-to-buy-and-sell-stock-with-transaction-fee.java
@@ -1,19 +1,21 @@
 class Solution {
-    int n,f;
-    public int helper(int[] prices, int i, int canbuy, int[][] dp) {
-        if(i>=n) return 0;
-        if(dp[i][canbuy]!=-1) return dp[i][canbuy];
-        if(canbuy==1) {
-            return dp[i][canbuy] = Math.max(helper(prices,i+1,1,dp),helper(prices,i+1,0,dp)-prices[i]);
+    int transaction_fee, n1;
+    int[][] dp;
+    public int helper(int[] prices, int ind, int can) {
+        if(ind==n1) return 0;
+        if(dp[ind][can]!=-1) return dp[ind][can];
+        if(can==1) {
+            return dp[ind][can]=Math.max(helper(prices,ind+1,1),-prices[ind]+helper(prices,ind+1,0));
         } else {
-            return dp[i][canbuy] = Math.max(helper(prices,i+1,0,dp),helper(prices,i+1,1,dp)+prices[i]-f);
+            return dp[ind][can]=Math.max(helper(prices,ind+1,0), prices[ind]+helper(prices,ind+1,1)-transaction_fee);
         }
     }
     public int maxProfit(int[] prices, int fee) {
-        f=fee;
-        n=prices.length;
-        int[][] dp = new int[n+1][2];
-        for(int i[]:dp) Arrays.fill(i,-1);
-        return helper(prices,0,1,dp);
+        int n=prices.length;
+        n1=n;
+        transaction_fee=fee;
+        dp = new int[n+1][2];
+        for(int[] i:dp) Arrays.fill(i,-1);
+        return helper(prices,0,1);
     }
 }
