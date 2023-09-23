371aa06f5940a1911d32722d820ecd8034a9fae2
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-23 15:45:20 +0530

    Time: 3 ms (96.26%), Space: 43.3 MB (92.94%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -1,22 +1,23 @@
 class Solution {
-    public int helper(char[] sarr, char[] tarr, int i, int j,int[][] dp) {
+    int[][] dp;
+    public int helper(char[] a, char[] b, int i, int j) {
         if(i==-1) return j+1;
         if(j==-1) return i+1;
         if(dp[i][j]!=-1) return dp[i][j];
-        if(sarr[i]==tarr[j]) {
-            return helper(sarr,tarr,i-1,j-1,dp);
+        if(a[i]==b[j]) {
+            return dp[i][j]=helper(a,b,i-1,j-1);
         }
-        int insert = 1 + helper(sarr,tarr,i,j-1,dp);
-        int delete = 1 + helper(sarr,tarr,i-1,j,dp);
-        int replace = 1+ helper(sarr,tarr,i-1,j-1,dp);
-        int res = Math.min(insert,Math.min(delete,replace));
-        return dp[i][j] = res;
+        int insert = 1 + helper(a,b,i,j-1);
+        int delete = 1 + helper(a,b,i-1,j);
+        int replace= 1 + helper(a,b,i-1,j-1);
+        int res=Math.min(insert,Math.min(delete,replace));
+        return dp[i][j]=res;
     }
-    public int minDistance(String word1, String word2) {
-        char[] sarr=word1.toCharArray(), tarr=word2.toCharArray();
-        int n1=sarr.length,n2=tarr.length;
-        int[][] dp = new int[n1+1][n2+1];
+    public int minDistance(String s, String t) {
+        char[] a=s.toCharArray(), b=t.toCharArray();
+        int n1=a.length,n2=b.length;
+        dp=new int[n1+1][n2+1];
         for(int[] i:dp) Arrays.fill(i,-1);
-        return helper(sarr,tarr,n1-1,n2-1,dp);
+        return helper(a,b,n1-1,n2-1);
     }
 }
