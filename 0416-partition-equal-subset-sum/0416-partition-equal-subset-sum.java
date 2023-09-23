781972b3a3d754de814bd3c60363bdf07b0ef8e5
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-23 11:16:18 +0530

    Time: 46 ms (47.02%), Space: 47 MB (45.21%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int sum=0, n=nums.length;
+        int sum=0,n=nums.length;
         for(int i:nums) sum+=i;
         if(sum%2==1) return false;
         sum/=2;
@@ -8,10 +8,8 @@ class Solution {
         dp[0][0]=true;
         for(int i=1;i<=n;i++) {
             for(int j=0;j<=sum;j++) {
-                boolean dont = dp[i-1][j];
-                boolean pick = false;
-                if(j-nums[i-1]>=0) pick = dp[i-1][j-nums[i-1]];
-                dp[i][j]=pick || dont;
+                dp[i][j]=dp[i-1][j];
+                if(j-nums[i-1]>=0 && dp[i][j]==false)dp[i][j]=dp[i-1][j-nums[i-1]];
             }
         }
         return dp[n][sum];
