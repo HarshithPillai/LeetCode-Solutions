09439b4c9cb84d215a3de7e0f03cbe7def72cfbd
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-23 15:52:01 +0530

    Time: 5 ms (68.02%), Space: 43.4 MB (75.89%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.java
+++ 0072-edit-distance/0072-edit-distance.java
@@ -17,7 +17,24 @@ class Solution {
         char[] a=s.toCharArray(), b=t.toCharArray();
         int n1=a.length,n2=b.length;
         dp=new int[n1+1][n2+1];
-        for(int[] i:dp) Arrays.fill(i,-1);
-        return helper(a,b,n1-1,n2-1);
+        for(int i=0;i<=n1;i++) {
+            for(int j=0;j<=n2;j++) {
+                if(i==0 && j==0) continue;
+                if(i==0) dp[i][j]=j;
+                else if(j==0) dp[i][j]=i;
+                else {
+                    if(a[i-1]==b[j-1]) {
+                        dp[i][j]=dp[i-1][j-1];
+                    } else {
+                        int insert = 1 + dp[i][j-1];
+                        int delete = 1 + dp[i-1][j];
+                        int replace= 1 + dp[i-1][j-1];
+                        int res=Math.min(insert,Math.min(delete,replace));
+                        dp[i][j]=res;
+                    }
+                }
+            }
+        }
+        return dp[n1][n2];
     }
 }
