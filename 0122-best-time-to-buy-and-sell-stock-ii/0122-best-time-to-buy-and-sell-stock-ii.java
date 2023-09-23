35c3af9e3411f8551fb2835f0f4b1518fb5bc87e
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-23 16:17:00 +0530

    Time: 4 ms (6.57%), Space: 48.5 MB (5.07%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -1,29 +1,18 @@
 class Solution {
-    public int helper(int[] prices, int i, int canBuy, int n, int[][] dp) {
-        if(i==n) return 0;
-        if(dp[i][canBuy]!=-1) return dp[i][canBuy];
-        int res=0;
+    int[][] dp;
+    public int helper(int[] prices, int ind, int canBuy, int n) {
+        if(ind==n) return 0;
+        if(dp[ind][canBuy]!=-1) return dp[ind][canBuy];
         if(canBuy==1) {
-            res=helper(prices,i+1,0,n, dp)-prices[i];
-            res=Math.max(res,helper(prices,i+1,1,n, dp));
+            return dp[ind][canBuy]=Math.max(helper(prices,ind+1,1,n), -prices[ind]+helper(prices,ind+1,0,n));
         } else {
-            res=prices[i] + helper(prices,i+1,1,n, dp);
-            res=Math.max(res,helper(prices,i+1,0,n, dp));
+            return dp[ind][canBuy]=Math.max(helper(prices,ind+1,0,n), prices[ind]+helper(prices,ind+1,1,n));
         }
-        return dp[i][canBuy]=res;
     }
     public int maxProfit(int[] prices) {
         int n=prices.length;
-        int[][] dp = new int[n+1][2];
-        for(int i=n-1;i>=0;i--) {
-            for(int j=0;j<2;j++) {
-                if(j==1){// can buy
-                    dp[i][j]=Math.max(dp[i+1][1],dp[i+1][0]-prices[i]);
-                } else {
-                    dp[i][j]=Math.max(dp[i+1][0],prices[i]+dp[i+1][1]);
-                }
-            }
-        }
-        return dp[0][1];
+        dp = new int[n+1][2];
+        for(int[] i:dp) Arrays.fill(i,-1);
+        return helper(prices, 0, 1, n);
     }
 }
