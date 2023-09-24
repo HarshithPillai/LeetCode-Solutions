0569ce7d15a81207581bb68667ecee75dc401cd0
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-24 11:03:13 +0530

    Time: 5 ms (99.17%), Space: 40.5 MB (97.3%) - LeetHub

--- 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
+++ 1121-partition-array-for-maximum-sum/1121-partition-array-for-maximum-sum.java
@@ -3,25 +3,18 @@ class Solution {
     public int helper(int[] arr, int s, int e, int k) {
         if(s>e) return 0;
         if(dp[s]!=-1) return dp[s];
-        int res=0, max=0;
-        for(int i=s;i<=e && (i-s+1)<=k ;i++) {
-            if(max<arr[i]) max=arr[i];
-            res=Math.max(res, max*(i-s+1) + helper(arr, i+1, e, k));
+        int max=arr[s], res=0;
+        for(int ind=s; ind<=e && (ind-s+1)<=k; ind++) {
+            if(arr[ind]>max) max=arr[ind];
+            int curr=max*(ind-s+1);
+            res=Math.max(res, curr + helper(arr, ind+1, e, k));
         }
         return dp[s]=res;
     }
     public int maxSumAfterPartitioning(int[] arr, int k) {
-        int n=arr.length,e=n-1;
-        int[] dp = new int[n+1];
-        // Arrays.fill(dp,-1);
-        for(int s=n-1;s>=0;s--) {
-            int res=0, max=0;
-            for(int i=s;i<=e && (i-s+1)<=k ;i++) {
-                if(max<arr[i]) max=arr[i];
-                res=Math.max(res, max*(i-s+1) + dp[i+1]);
-            }
-            dp[s]=res;
-        }
-        return dp[0];
+        int n=arr.length;
+        dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return helper(arr, 0, n-1, k);
     }
 }
