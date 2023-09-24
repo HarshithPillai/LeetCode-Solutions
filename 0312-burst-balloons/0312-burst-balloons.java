7a963a3a282fe9536af415b31c1434bd5e2022ee
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-24 09:50:57 +0530

    Time: 96 ms (56.82%), Space: 41.7 MB (36.52%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.java
+++ 0312-burst-balloons/0312-burst-balloons.java
@@ -4,34 +4,24 @@ class Solution {
         if(s>e) return 0;
         if(dp[s][e]!=-1) return dp[s][e];
         int res=0;
-        for(int k=s;k<=e;k++) {
-            int current = nums[s-1]*nums[k]*nums[e+1];
-            int left    = helper(nums, s, k-1);
-            int right   = helper(nums, k+1, e);
-            res = Math.max(res, current + left + right);
+        for(int k=s; k<=e; k++) {
+            int curr = nums[s-1] * nums[k] * nums[e+1];
+            int left = helper(nums, s, k-1);
+            int right= helper(nums, k+1, e);
+            res = Math.max(res, curr+left+right);
         }
-        return dp[s][e] = res;
+        return dp[s][e]=res;
     }
     public int maxCoins(int[] nums) {
+        /**
+            1,x,c,s,g,c,s,g,1
+        */
         int n=nums.length;
-        dp=new int[n+2][n+2];
-        //for(int[] i:dp) Arrays.fill(i,-1);
-        int[] newnums = new int[n+2];
+        int[] newnums=new int[n+2];
         newnums[0]=1; newnums[n+1]=1;
         for(int i=1;i<=n;i++) newnums[i]=nums[i-1];
-
-        for(int s=n;s>=1;s--) {
-            for(int e=s;e<=n;e++) {
-                int res=0;
-                for(int k=s;k<=e;k++) {
-                    int current = newnums[s-1]*newnums[k]*newnums[e+1];
-                    int left    = dp[s][k-1];
-                    int right   = dp[k+1][e];
-                    res = Math.max(res, current + left + right);
-                }
-                dp[s][e] = res;
-            }
-        }
-        return dp[1][n];
+        dp = new int[n+2][n+2];
+        for(int i[]: dp) Arrays.fill(i,-1);
+        return helper(newnums, 1, n);
     }
 }
