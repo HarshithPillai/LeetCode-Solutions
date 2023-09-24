ad14ebe3f8c5e69cdef513c30e1a66432cb5bd8b
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-24 11:31:26 +0530

    Time: 32 ms (12.53%), Space: 43.9 MB (72.98%) - LeetHub

--- 0085-maximal-rectangle/0085-maximal-rectangle.java
+++ 0085-maximal-rectangle/0085-maximal-rectangle.java
@@ -1,40 +1,41 @@
 class Solution {
     public int maximalRectangle(char[][] matrix) {
-        int r=matrix.length, c=matrix[0].length;
+        int r = matrix.length, c = matrix[0].length;
+        int[] histogram = new int[c];
         int max=0;
-        int[] row = new int[c];
-        int[] prev_smaller = new int[c];
-        int[] next_smaller = new int[c];
-        for(int i=0;i<r;i++) {
-            Stack<Integer> stack=new Stack<>();
+        for(int i=0; i<r; i++) {
+            Stack<Integer> stack = new Stack<>();
+            int[] prev_smaller = new int[c];
+            int[] next_smaller = new int[c];
             for(int j=0;j<c;j++) {
                 if(matrix[i][j]=='1') {
-                    row[j]++;
+                    histogram[j]++;
                 } else {
-                    row[j]=0;
+                    histogram[j]=0;
                 }
-                while(!stack.isEmpty() && row[stack.peek()]>=row[j]) {
+                while(!stack.isEmpty() && histogram[stack.peek()]>=histogram[j]) {
                     stack.pop();
                 }
                 if(stack.isEmpty()) {
-                    prev_smaller[j]=-1;
+                    prev_smaller[j] = -1;
                 } else {
-                    prev_smaller[j]=stack.peek();
+                    prev_smaller[j] = stack.peek();
                 }
                 stack.push(j);
             }
-            stack=new Stack<>();
+            stack = new Stack<>();
             for(int j=c-1;j>=0;j--) {
-                while(!stack.isEmpty() && row[stack.peek()]>=row[j]) {
+                while(!stack.isEmpty() && histogram[stack.peek()]>=histogram[j]) {
                     stack.pop();
                 }
                 if(stack.isEmpty()) {
-                    next_smaller[j]=c;
+                    next_smaller[j] = c;
                 } else {
-                    next_smaller[j]=stack.peek();
+                    next_smaller[j] = stack.peek();
                 }
-                max=Math.max(max,row[j]*(next_smaller[j]-prev_smaller[j]-1));
                 stack.push(j);
+
+                max=Math.max(max, histogram[j]*(next_smaller[j]-prev_smaller[j]-1));
             }
         }
         return max;
