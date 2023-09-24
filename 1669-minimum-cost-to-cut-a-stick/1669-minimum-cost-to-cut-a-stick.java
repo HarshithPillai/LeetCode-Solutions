1798b3727c7667ff696b4b0f7a342fcf8a77880a
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-24 08:27:39 +0530

    Time: 19 ms (73.79%), Space: 41.8 MB (28.8%) - LeetHub

--- 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
+++ 1669-minimum-cost-to-cut-a-stick/1669-minimum-cost-to-cut-a-stick.java
@@ -1,43 +1,29 @@
 class Solution {
     int[][] dp;
     public int helper(int[] cuts, int s, int e) {
+        //if(s+1==e) return 0;
         if(s>e) return 0;
         if(dp[s][e]!=-1) return dp[s][e];
-        int res = Integer.MAX_VALUE;
-        for(int i=s;i<=e;i++) {
-            int current = cuts[e+1] - cuts[s-1];
-            int first   = helper(cuts,  s, i-1);
-            int second  = helper(cuts, i+1,  e);
-            res = Math.min(res,current + first + second);
+        int curr = cuts[e+1]-cuts[s-1], res=(int)(1e9);
+        for(int k=s; k<=e; k++) {
+            int left = helper(cuts,s,k-1);
+            int right= helper(cuts,k+1,e);
+            res=Math.min(res, curr+left+right);
         }
         return dp[s][e]=res;
     }
-    public int minCost(int n, int[] cuts) {
-        Arrays.sort(cuts); // needed to make each subproblem separate from each other
-        // other wise we might get a case [1,2,6] [3,5]. 
-        // here the entire 2nd array is within the range of first array
-        int size=cuts.length;
-        dp=new int[size+2][size+2];
-        
-        int[] newcuts= new int[size+2];
-        for(int i=1;i<=size;i++) {
-            newcuts[i]=cuts[i-1];
-        }
-        newcuts[size+1]=n;
-
-        for(int i=size; i>0; i--) {
-            for(int j=i;j<=size;j++) {
-                int res=Integer.MAX_VALUE;
-                for(int k=i;k<=j;k++) {
-                    int current = newcuts[j+1] - newcuts[i-1];
-                    int first   = dp[i][k-1];
-                    int second  = dp[k+1][j];
-                    res = Math.min(res,current + first + second);
-                }
-                dp[i][j]=res;
-            }
-        }
-
-        return dp[1][size];
+    public int minCost(int len, int[] cuts) {
+        /**
+            0,1,3,4,5,len
+            f(s,e) = min(curr, k:(s->e)->{cuts[e+1]-cuts[s-1] + f(s,k-1) + f(k+1,e)})
+         */
+        int n=cuts.length;
+        Arrays.sort(cuts);
+        int[] newcuts = new int[n+2];
+        newcuts[0]=0; newcuts[n+1]=len;
+        for(int i=1;i<=n;i++) newcuts[i]=cuts[i-1];
+        dp = new int[n+2][n+2];
+        for(int[] i:dp) Arrays.fill(i,-1);
+        return helper(newcuts, 1, n);
     }
 }
