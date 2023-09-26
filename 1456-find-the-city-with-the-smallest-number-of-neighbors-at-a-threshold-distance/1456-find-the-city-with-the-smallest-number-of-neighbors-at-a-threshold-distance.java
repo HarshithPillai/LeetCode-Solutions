54dba616b460be494d54f009aff577cbacc83c3a
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-26 12:50:42 +0530

    Time: 45 ms (24.93%), Space: 43.5 MB (38.43%) - LeetHub

--- 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.java
@@ -1,32 +1,46 @@
 class Solution {
+    class Pair {
+        int first, second;
+        Pair(int f, int s) { first=f; second=s; }
+    }
+    public void dijkstra(List<List<Pair>> adj, int[] dist, int start) {
+        dist[start]=0;
+        PriorityQueue<Pair> pq = new PriorityQueue<>((x,y)->x.first-y.first);
+        pq.add(new Pair(0,start)); // dis, node
+        while(!pq.isEmpty()) {
+            Pair top = pq.poll();
+            int dis = top.first;
+            int node = top.second;
+            for(Pair nbr:adj.get(node)) {
+                int next = nbr.first;
+                int wt   = nbr.second;
+                if(dist[next] > dis + wt) {
+                    dist[next] = dis + wt;
+                    pq.add(new Pair(dis + wt, next));
+                }
+            }
+        }
+    }
     public int findTheCity(int n, int[][] edges, int tar) {
-        int[][] matrix = new int[n][n];
         int m = edges.length;
-        for(int i[]:matrix) Arrays.fill(i,(int)(1e9));
-        for(int i=0;i<n;i++) matrix[i][i]=0;
+        List<List<Pair>> adj = new ArrayList<>();
+        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
         for(int i=0; i<m; i++) {
             int u=edges[i][0];
             int v=edges[i][1];
             int w=edges[i][2];
-            matrix[u][v]=w;
-            matrix[v][u]=w;
-        }
-
-        for(int via=0;via<n;via++) {
-            for(int i=0;i<n;i++) {
-                for(int j=0;j<n;j++) {
-                    if(matrix[i][j] > matrix[i][via] + matrix[via][j]) {
-                        matrix[i][j] = matrix[i][via] + matrix[via][j];
-                    }
-                }
-            }
+            adj.get(u).add(new Pair(v,w));
+            adj.get(v).add(new Pair(u,w));
         }
 
         int max=Integer.MAX_VALUE, res=-1;
         for(int i=0;i<n;i++) {
             int count=0;
+            int[] dist = new int[n];
+            Arrays.fill(dist,(int)(1e9));
+            dijkstra(adj, dist, i);
             for(int j=0;j<n;j++) {
-                if(matrix[i][j]<=tar) {
+                if(dist[j]<=tar) {
                     count++;
                 }
             }
