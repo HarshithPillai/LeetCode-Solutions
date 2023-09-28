9ebe612541cf5d1d7e4408c48ce97ab1fe58f1ec
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-09-28 12:27:26 +0530

    Time: 72 ms (91.58%), Space: 119.3 MB (41.29%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,34 +1,37 @@
 class Solution {
-    int timer = 0;
-    public void dfs(int[] t_in, int[] low, int[] vis, List<List<Integer>> adj, int node, int parent, List<List<Integer>> result) {
-        vis[node]=1;
+    int timer = 1;
+    public void dfs(List<List<Integer>> adj, int node, int parent, int[] t_in, int[] low, List<List<Integer>> res) {
+        low[node]=timer;
         t_in[node]=timer;
-        low[node]=timer++;
+        timer++;
         for(int nbr:adj.get(node)) {
             if(parent==nbr) continue;
-            if(vis[nbr]==0) {
-                dfs(t_in, low, vis, adj, nbr, node, result);
-                low[node]=Math.min(low[node], low[nbr]);
-                // check if this edge can be a critical bridge
-                if(t_in[node]<low[nbr]) {
-                    List<Integer> temp = new ArrayList<>();
-                    temp.add(node); temp.add(nbr);
-                    result.add(temp);
+            if(t_in[nbr]==0) {
+                dfs(adj, nbr, node, t_in, low, res);
+                low[node]=Math.min(low[node],low[nbr]);
+                if(t_in[node] < low[nbr]) {
+                    List<Integer> t=new ArrayList<>();
+                    t.add(node);t.add(nbr);
+                    res.add(t);
                 }
-            } else if(low[nbr]<low[node]) low[node]=low[nbr];
+            } else {
+                low[node]=Math.min(low[node],low[nbr]);
+            }
         }
     }
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        int[] t_in = new int[n], low = new int[n], vis = new int[n];
+        int t_in[] = new int[n];
+        int  low[] = new int[n];
+        List<List<Integer>> res = new ArrayList<>();
         List<List<Integer>> adj = new ArrayList<>();
         for(int i=0;i<n;i++) adj.add(new ArrayList<>());
         for(int i=0;i<connections.size();i++) {
-            int u=connections.get(i).get(0), v=connections.get(i).get(1);
-            adj.get(u).add(v); adj.get(v).add(u);
+            int u = connections.get(i).get(0);
+            int v = connections.get(i).get(1);
+            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
-        List<List<Integer>> result = new ArrayList<>();
-        
-        dfs(t_in, low, vis, adj, 0, -1, result);
-        return result;
+        dfs(adj, 0, -1, t_in, low, res);
+        return res;
     }
 }
