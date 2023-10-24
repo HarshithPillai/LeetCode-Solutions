783bc3433bc413e7946a446d1e0bd2a610f483eb
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-10-24 13:40:27 +0530

    Time: 10 ms (48.68%), Space: 39.4 MB (97.28%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.java
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -1,34 +1,20 @@
 class Solution {
-    public int[] getXOfGrid(int i, int j) {
-        int x=i/3;
-        int y=j/3;
-        int[] res=new int[2];
-        res[0]=x;res[1]=y;
-        return res;
-    }
-    public boolean canPlace(char[][] board, int i, int j, char val) {
-        for(int row=0; row<9; row++) {
-            if(board[row][j]==val) return false;
-            if(board[i][row]==val) return false;
-            if(board[(i/3)*3+(row/3)][(j/3)*3+(row%3)]==val) return false;
+    public boolean check(char[][] board, int row, int col, char ch) {
+        for(int i=0; i<9; i++) {
+            if(board[i][col] == ch || board[row][i] == ch) return false;
+            if(board[(row/3)*3 + i/3][(col/3)*3 + i%3] == ch) return false;
         }
-        //4,7
-        
         return true;
     }
-    public boolean recUtil(char[][] board) {
-        
+    public boolean solveSudokuRec(char[][] board) {
         for(int i=0; i<9; i++) {
             for(int j=0; j<9; j++) {
                 if(board[i][j]=='.') {
-                    for(char ch='1'; ch<='9'; ch++) {
-                        if(canPlace(board, i,j, ch)) {
-                            board[i][j]=ch;
-                            if(recUtil(board)) {
-                                return true;
-                            } else {
-                                board[i][j]='.';
-                            }
+                    for(char ch = '1'; ch<='9'; ch++) {
+                        if(check(board, i, j, ch)) {
+                            board[i][j] = ch;
+                            if(solveSudokuRec(board)) return true;
+                            else board[i][j] = '.';
                         }
                     }
                     return false;
@@ -38,7 +24,6 @@ class Solution {
         return true;
     }
     public void solveSudoku(char[][] board) {
-        
-        recUtil(board);
+        solveSudokuRec(board);
     }
 }
