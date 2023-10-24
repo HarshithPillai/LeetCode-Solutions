658815d29a23e0d5a54da9d9b2f807615f5f4870
HarshithPillai <42529664+HarshithPillai@users.noreply.github.com>
2023-10-24 13:06:35 +0530

    Time: 2 ms (89.81%), Space: 43.5 MB (94.32%) - LeetHub

--- 0051-n-queens/0051-n-queens.java
+++ 0051-n-queens/0051-n-queens.java
@@ -1,59 +1,56 @@
 class Solution {
-    public boolean canPlace(int i, int j, char[][] board, int n) {
-        int x=i, y=j;
-        if(i==n || i==-1 || j==-1 || j==n) return false;
-        
-        while(i<n && j<n && i>-1 && j>-1) {
-            if(board[i][j]=='Q') return false;
-            i++; j--;
+    public List<String> convert(boolean[][] grid, int n) {
+        // TODO
+        List<String> res = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            char[] arr = new char[n];
+            for(int j=0; j<n; j++) {
+                if(grid[i][j]) arr[j] = 'Q';
+                else arr[j] = '.';
+            }
+            res.add(new String(arr));
         }
-        i=x;
-        j=y;
-        while(i<n && j<n && i>-1 && j>-1) {
-            if(board[i][j]=='Q') return false;
+        return res;
+    }
+    public boolean check(boolean[][] grid, int row, int col, int n) {
+        int i=row-1, j=col-1;
+        // diagonally left up
+        while(i>=0 && j>=0) {
+            if(grid[i][j]) return false;
             i--; j--;
         }
-        i=x;
-        j=y;
-        while(j>-1) {
-            if(board[i][j]=='Q') return false;
+        i=row; j=col-1;
+        while(j>=0) {
+            if(grid[i][j]==true) return false;
             j--;
         }
+        i=row+1; j=col-1;
+        while(i<n && j>=0) {
+            if(grid[i][j]==true) return false;
+            i++; j--;
+        }
+
         return true;
     }
-    public void recUtil(List<List<String>> result, char[][] board, int ind, int n) {
-        if(ind==n) {
-            // all queens are placed on the board. TODO add the strings to a list and add the list to result
-            List<String> temp = new ArrayList<>();
-            for(int i=0; i<n; i++) {
-                String row="";
-                for(int j=0; j<n; j++) {
-                    row+=""+board[i][j];
-                }
-                temp.add(row);
-            }
-            result.add(temp);
-            return;
+    public void dfs(boolean[][] grid, int col, int n, List<List<String>> result) {
+        if(col == n) {
+            List<String> res = convert(grid, n);
+            result.add(res);
         }
-        for(int i=0; i<n; i++) {
-            if(canPlace(i, ind, board, n)) {
-                board[i][ind] = 'Q';
-                recUtil(result, board, ind+1, n);
-                board[i][ind] = '.';
+        for(int row = 0; row<n; row++) {
+            if(check(grid, row, col, n)) {
+                grid[row][col] = true;
+                dfs(grid, col+1, n, result);
+                grid[row][col] = false;
             }
         }
     }
-
     public List<List<String>> solveNQueens(int n) {
+        // table structure: List<boolean>
+        boolean[][] temp = new boolean[n][n];
         List<List<String>> result = new ArrayList<>();
-        char[][] board = new char[n][n];
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<n; j++) {
-                board[i][j]='.';
-            }
-        }
-        
-        recUtil(result, board, 0, n);
+
+        dfs(temp, 0, n, result);
 
         return result;
     }
