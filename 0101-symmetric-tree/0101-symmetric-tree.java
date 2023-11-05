8a9829bd6239c3806316a2dfe82ae509e3db84f0
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2023-11-05 14:50:26 +0530

    Time: 0 ms (100%), Space: 40 MB (95.78%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,16 +14,13 @@
  * }
  */
 class Solution {
-    public boolean dfs(TreeNode nl, TreeNode nr) {
-        if(nl==null && nr==null) return true;
-        if(nl==null && nr!=null) return false;
-        if(nl!=null && nr==null) return false;
-        if(nl.val != nr.val) return false;
-        if(dfs(nl.left, nr.right)==false) return false;
-        if(dfs(nl.right, nr.left)==false) return false;
-        return true;
+    public boolean isSymmetric(TreeNode root1,TreeNode root2) {
+        if(root1==null && root2==null) return true;
+        if(root1==null || root2==null || root1.val!=root2.val) return false;
+        return isSymmetric(root1.left, root2.right) && isSymmetric(root1.right, root2.left);
     }
     public boolean isSymmetric(TreeNode root) {
-        return dfs(root.left, root.right);
+        if(root==null) return false;
+        return isSymmetric(root.left, root.right);
     }
 }
