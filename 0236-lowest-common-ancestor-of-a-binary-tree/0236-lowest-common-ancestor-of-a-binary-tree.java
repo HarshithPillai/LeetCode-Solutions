343373a2ef69ce167f031a3024c9a38a81a1d57a
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2023-11-05 15:14:29 +0530

    Time: 6 ms (97.5%), Space: 43.2 MB (86.55%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,22 +8,18 @@
  * }
  */
 class Solution {
-    TreeNode result = null;
-    public int recutil(TreeNode root, TreeNode p, TreeNode q) {
+    TreeNode res = null;
+    public int dfs(TreeNode root, TreeNode p, TreeNode q) {
         if(root==null) return 0;
-        int left = recutil(root.left, p, q);
-        int right = recutil(root.right, p, q);
+        int left = dfs(root.left, p, q);
+        int right = dfs(root.right,p,q);
         int curr=0;
-        if(root==q || root==p) {
-            if(left+right==1 && result==null) result=root;
-            else curr=1;
-        }
-        if(left+right==2 && result==null) result=root;
-        
+        if(root==p || root==q) curr=1;
+        if(left+right+curr==2 && res==null) res = root;
         return left+right+curr;
     }
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        recutil(root, p, q);
-        return result;
+        dfs(root, p, q);
+        return res;
     }
 }
