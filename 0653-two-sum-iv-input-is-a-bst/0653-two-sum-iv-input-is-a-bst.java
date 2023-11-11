d61728ef185e0b74b6096b307968a84cc2d08f5d
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2023-11-11 09:20:30 +0530

    Time: 3 ms (48.02%), Space: 44 MB (30.84%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -14,20 +14,62 @@
  * }
  */
 class Solution {
-    boolean ans=false;
-    public void recutil(TreeNode root, int k, Set<Integer> set) {
-        if(root==null) return;
-        recutil(root.left, k, set);
-        if(set.contains(k-root.val)) {
-            ans=true;
-            return;
+    Stack<TreeNode> beforeSt, nextSt;
+    public boolean hasNext() {
+        return (!nextSt.isEmpty());
+    }
+    public boolean hasPrev() {
+        return (!beforeSt.isEmpty());
+    }
+    public int next() {
+        TreeNode curr = nextSt.pop();
+        int val = curr.val;
+        if(curr.right!=null) {
+            curr=curr.right;
+            while(curr!=null) {
+                nextSt.push(curr);
+                curr=curr.left;
+            }
         }
-        set.add(root.val);
-        recutil(root.right, k, set);
+        return val;
+    }
+    public int prev() {
+        TreeNode curr = beforeSt.pop();
+        int val = curr.val;
+        if(curr.left!=null) {
+            curr=curr.left;
+            while(curr!=null) {
+                beforeSt.push(curr);
+                curr=curr.right;
+            }
+        }
+        return val;
     }
     public boolean findTarget(TreeNode root, int k) {
-        Set<Integer> set=new HashSet<>();
-        recutil(root, k, set);
-        return ans;
+        nextSt = new Stack<>();
+        beforeSt = new Stack<>();
+        TreeNode curr = root;
+        while(curr!=null) {
+            nextSt.push(curr);
+            curr=curr.left;
+        }
+        curr=root;
+        while(curr!=null) {
+            beforeSt.push(curr);
+            curr=curr.right;
+        }
+        int left = next();
+        int right = prev();
+        while(left<right) {
+            if(left+right==k) return true;
+            else if(left+right>k) {
+                if(hasPrev()) right=prev();
+                else return false;
+            } else {
+                if(hasNext()) left=next();
+                else return false;
+            }
+        }
+        return false;
     }
 }
