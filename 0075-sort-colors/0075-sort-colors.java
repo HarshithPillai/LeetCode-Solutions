8cf2969af2f66b776b7e0e5989ce572c97bbe133
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-06-12 13:42:14 +0530

    Time: 290 ms (6.79%), Space: 45.5 MB (9.24%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,36 +1,23 @@
 class Solution {
-    public void swap(int[] nums, int low, int high) {
-        int a=nums[low];
-        nums[low] = nums[high];
-        nums[high]=a;
-    }
     public void sortColors(int[] nums) {
-        //int i=0, j=0, k=0;
-        // for(int a=0; a<nums.length; a++) {
-        //     if(nums[a]==0) i++;
-        //     else if(nums[a]==1) j++;
-        // }
-        // for(int a=0; a<nums.length; a++) {
-        //     if(i==0 && j==0) nums[a]=2;
-        //     else if(i==0) {nums[a]=1; j--;}
-        //     else {nums[a]=0; i--;}
-        // }
-        int n=nums.length;
-        if(n==1) return;
-
-
-        int low=0, mid=0, high=n-1;
-        while(mid<=high) {
-            if(nums[mid]==0) {
-                swap(nums, mid, low);
-                mid++;
-                low++;
-            } else if(nums[mid]==1) {
-                mid++;
+        int i=0, j=0, k=nums.length-1;
+        while(j<=k && i<=k) {
+            if(j<i) j=i;
+            // while(i<j && nums[i] == 0) i++;
+            // while(k>j && nums[k] == 2) k--;
+            if(nums[j] == 0) {
+                nums[j] = nums[i];
+                nums[i] = 0;
+                i++;
+            } else if(nums[j] == 2) {
+                nums[j] = nums[k];
+                nums[k] = 2;
+                k--;
             } else {
-                swap(nums, high, mid);
-                high--;
+                j++;
             }
+            for(int num:nums) System.out.print(num+",");
+            System.out.println(": "+i+" "+j+" "+k+" ");
         }
     }
 }
