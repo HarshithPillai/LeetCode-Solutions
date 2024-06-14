010f0e455c90badfbf865e9aa7aee6e4c7acbfbe
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-06-14 13:51:12 +0530

    Time: 9 ms (84.92%), Space: 61.5 MB (5.25%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.java
@@ -4,13 +4,23 @@ class Solution {
             1 1 2 2 3 7
             1 2 3 4 5 7
         */
-        Arrays.sort(nums);
         int ans = 0;
-        for(int i=1; i<nums.length; i++) {
-            if(nums[i] <= nums[i-1]) {
-                ans += nums[i-1] - nums[i] + 1;
-                nums[i] = nums[i-1] + 1;
+        int freq[] = new int[100001];
+        for(int num:nums) {
+            freq[num]++;
+        }
+        int last = -1;
+        for(int i=0; i<freq.length; i++) {
+            // last = Math.max(last, i);
+            if(freq[i]==0) {
+                continue;
             }
+            if(last>=i) {
+                ans+=(last-i+1)*freq[i];
+            }
+            ans+=((freq[i]-1)*freq[i])/2;
+            if(last>=i) last = last+freq[i];
+            else last = i+freq[i]-1;
         }
         return ans;
     }
