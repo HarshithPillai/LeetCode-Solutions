73963857439f1b186514446a7d740389be491277
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-06-14 14:05:16 +0530

    Time: 93 ms (92.72%), Space: 74 MB (13.98%) - LeetHub

--- 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
+++ 0982-minimum-increment-to-make-array-unique/0982-minimum-increment-to-make-array-unique.cpp
@@ -2,22 +2,24 @@ class Solution {
 public:
     int minIncrementForUnique(vector<int>& nums) {
         int ans = 0;
-        vector<int> freq(100001, 0);
+        int maxn = 0;
+        for(int num : nums) maxn = max(maxn, num);
+        vector<int> freq(nums.size() + maxn + 1, 0);
         for(int num : nums) {
             freq[num]++;
         }
         int last = -1;
-        for(int i=0; i<freq.size(); i++) {
+        for(int i = 0; i < freq.size(); i++) {
             // last = max(last, i);
-            if(freq[i]==0) {
+            if(freq[i] == 0) {
                 continue;
             }
-            if(last>=i) {
-                ans+=(last-i+1)*freq[i];
+            if(last >= i) {
+                ans += (last - i + 1) * freq[i];
             }
-            ans+=((freq[i]-1)*freq[i])/2;
-            if(last>=i) last = last+freq[i];
-            else last = i+freq[i]-1;
+            ans += ((freq[i] - 1) * freq[i]) / 2;
+            if(last >= i) last = last + freq[i];
+            else last = i + freq[i] - 1;
         }
         return ans;
     }
