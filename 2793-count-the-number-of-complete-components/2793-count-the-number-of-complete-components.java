e5223636dc82093dd3434cd7672e0dec12368212
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-07-17 11:30:20 +0530

    Time: 582 ms (5.42%), Space: 45.1 MB (32.31%) - LeetHub

---
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -0,0 +1,41 @@
+class Solution {
+    int ans = 0, curr = 0;
+    Set<Integer> set;
+    int[] vis;
+    boolean valid = true;
+    public void dfs(List<List<Integer>> adj, int node) {
+        vis[node] = 1;
+        for(int nbr:adj.get(node)) {
+            if(vis[nbr] == 0) {
+                if(!set.contains(node)) valid = false;
+                dfs(adj, nbr);
+            }
+        }
+        if(adj.get(node).size() != curr || !set.contains(node)) valid = false;
+    }
+    public int countCompleteComponents(int n, int[][] edges) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        for(int[] edge:edges) {
+            int u = edge[0], v = edge[1];
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        vis = new int[n];
+        for(int i=0; i<n; i++) {
+            for(int nbr:adj.get(i)) {
+                System.out.print(nbr + " ");
+            }
+            System.out.println("");
+            if(vis[i] == 0) {
+                curr = adj.get(i).size();
+                set = new HashSet<>(adj.get(i));
+                set.add(i);
+                dfs(adj, i);
+                if(valid) ans++;
+                valid = true;
+            }
+        }
+        return ans;
+    }
+}
