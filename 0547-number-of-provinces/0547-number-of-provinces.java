cd33baf32335a82abef7ec0d748707d1f2d47bfc
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-07-17 11:45:23 +0530

    Time: 4 ms (22.01%), Space: 48.1 MB (10.57%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -1,21 +1,44 @@
 class Solution {
-    public void recutil(int[][] isConnected, int node, int[] visited, int n) {
-        visited[node]=1;
-        for(int i=0; i<n; i++) {
-            if(isConnected[i][node]==1 && visited[i]==0) recutil(isConnected, i, visited, n);
-        }
-    }
-    public int findCircleNum(int[][] isConnected) {
-        int n = isConnected.length;
-        int components=0;
-        int[] visited = new int[n];
-
-        for(int i=0; i<n; i++) {
-            if(visited[i]==0) {
-                components++;
-                recutil(isConnected, i, visited, n);
+    class DisjointSet {
+        List<Integer> parent, size;
+        public DisjointSet(int n) {
+            parent = new ArrayList<>();
+            size = new ArrayList<>();
+            for(int i=0; i<n; i++) {
+                parent.add(i);
+                size.add(1);
             }
         }
-        return components;
+        public int findUPar(int node) {
+            int par = parent.get(node);
+            if(par != node) {
+                par = findUPar(par);
+                parent.set(node, par);
+            }
+            return par;
+        }
+        public boolean unionBySize(int u, int v) {
+            int parU = findUPar(u), parV = findUPar(v), sizeU = size.get(parU), sizeV = size.get(parV);
+            if(parU == parV) return false;
+            if(sizeU < sizeV) {
+                parent.set(parU, parV);
+                size.set(parV, sizeU + sizeV);
+            } else {
+                parent.set(parV, parU);
+                size.set(parU, sizeU + sizeV);
+            }
+            return true;
+        }
+    }
+    public int findCircleNum(int[][] edges) {
+        int n = edges.length, ans = 0;
+        DisjointSet ds = new DisjointSet(n);
+        for(int i=0; i<n; i++) {
+            for(int j = 0; j<n; j++) {
+                if(edges[i][j] == 1) ds.unionBySize(i, j);
+            }
+        }
+        for(int i = 0; i<n; i++) if(ds.parent.get(i) == i) ans++;
+        return ans;
     }
 }
