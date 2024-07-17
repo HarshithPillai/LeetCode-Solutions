42fa89b848c244c8de9b94bb34e5d338d58bf0ea
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-07-17 12:02:33 +0530

    Time: 4 ms (48.91%), Space: 48.9 MB (86.49%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,59 +1,30 @@
 class Solution {
-    class DisjointSet {
-        List<Integer> parent, size;
-        public DisjointSet(int n) {
-            parent = new ArrayList<>();
-            size = new ArrayList<>();
-            for(int i=0; i<n; i++) {
-                parent.add(i);
-                size.add(1);
-            }
-        }
-        public int findUPar(int node) {
-            int par = parent.get(node);
-            if(par != node) {
-                par = findUPar(par);
-                parent.set(node, par);
-            }
-            return par;
-        }
-        public boolean unionBySize(int u, int v) {
-            int parU = findUPar(u), parV = findUPar(v), sizeU = size.get(parU), sizeV = size.get(parV);
-            if(parU == parV) return false;
-            if(sizeU < sizeV) {
-                parent.set(parU, parV);
-                size.set(parV, sizeU + sizeV);
-            } else {
-                parent.set(parV, parU);
-                size.set(parU, sizeU + sizeV);
-            }
-            return true;
-        }
-    }
+    int[] dx = {1,0,-1,0}, dy = {0,-1,0,1};
+    int n, m;
     int getIndex(int i, int j, int m) {
         return i*m + j;
     }
-    public int numIslands(char[][] grid) {
-        int n = grid.length, m = grid[0].length;
-        DisjointSet ds = new DisjointSet(m*n);
-        int[] dx = {1,0,-1,0}, dy = {0,-1,0,1};
-        for(int i = 0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(grid[i][j] == '0') continue;
-                for(int k=0; k<4; k++) {
-                    int newr = i+dx[k], newc = j+dy[k];
-                    if(newr<n && newc<m && newr>=0 && newc>=0 && grid[newr][newc] == '1') {
-                        ds.unionBySize(getIndex(i, j, m), getIndex(newr, newc, m));
-                    }
-                }
+    public void dfs(char[][] grid, int i, int j, int[][] vis) {
+        vis[i][j] = 1;
+        for(int k=0; k<4; k++) {
+            int newr = i+dx[k], newc = j+dy[k];
+            if(newr<n && newc<m && newr>=0 && newc>=0 && grid[newr][newc] == '1' && vis[newr][newc] == 0) {
+                dfs(grid, newr, newc, vis);
             }
         }
+    }
+    public int numIslands(char[][] grid) {
+        n = grid.length;
+        m = grid[0].length;
         int ans = 0;
+        int[][] vis = new int[n][m];
         for(int i = 0; i<n; i++) {
             for(int j=0; j<m; j++) {
                 if(grid[i][j] == '0') continue;
-                int ind = getIndex(i, j, m);
-                if(ds.parent.get(ind) == ind) ans++;
+                if(vis[i][j] == 0) {
+                    ans++;
+                    dfs(grid, i, j, vis);
+                }
             }
         }
         return ans;
