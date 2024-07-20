8aec96645d866d37a44f4950ecc8d6c988645326
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-07-21 01:27:17 +0530

    Time: 33 ms (72.43%), Space: 57.6 MB (41.45%) - LeetHub

---
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -0,0 +1,35 @@
+class Solution {
+    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
+        double dist[] = new double[n];
+        Queue<double[]> q = new LinkedList<>();
+        List<List<double[]>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        int ind=0;
+        for(int[] ed:edges) {
+            int u = ed[0];
+            int v = ed[1];
+            double w = succProb[ind];
+            ind++;
+            adj.get(u).add(new double[]{v, w});
+            adj.get(v).add(new double[]{u, w});
+        }
+        q.add(new double[]{start_node, 1});
+        double[] ans = new double[n];
+        dist[start_node] = 1;
+        while(!q.isEmpty()) {
+            double[] top = q.poll();
+            int node = (int)top[0];
+            double w = top[1];
+            // if(node == end_node) return w;
+            for(double[] next:adj.get(node)) {
+                int nbr = (int)next[0];
+                double weight = next[1];
+                if(w*weight > dist[nbr]) {
+                    dist[nbr] = w*weight;
+                    q.add(new double[]{nbr, dist[nbr]});
+                }
+            }
+        }
+        return dist[end_node];
+    }
+}
