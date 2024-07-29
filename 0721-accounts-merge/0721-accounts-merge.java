320f96c40f306648fb1e11c33f044d46a4c5319b
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-07-29 18:57:33 +0530

    Time: 27 ms (91.11%), Space: 47.5 MB (63.46%) - LeetHub

--- 0721-accounts-merge/0721-accounts-merge.java
+++ 0721-accounts-merge/0721-accounts-merge.java
@@ -1,69 +1,70 @@
 class Solution {
-    class DisjointSet {
-        List<Integer> parent = new ArrayList<>(), size = new ArrayList<>();
+    public class DisjointSet {
+        List<Integer> parent, size;
         DisjointSet(int n) {
-            for(int i=0;i<=n;i++) {
-                parent.add(i); size.add(1);
+            parent = new ArrayList<>();
+            size = new ArrayList<>();
+            for(int i=0; i<n; i++) {
+                parent.add(i);
+                size.add(1);
             }
         }
-        public int findUPar(int u) {
-            if(parent.get(u)==u) return u;
-            int ulp_u = findUPar(parent.get(u));
-            parent.set(u, ulp_u);
-            return ulp_u;
+        int findUPar(int node) {
+            int par = parent.get(node);
+            if(par != node) {
+                par = findUPar(par);
+                parent.set(node, par);
+            }
+            return par;
         }
-        public void unionBySize(int u, int v) {
-            int ulp_u = findUPar(u), ulp_v = findUPar(v);
-            if(ulp_u == ulp_v) return;
-            if(size.get(ulp_u)>size.get(ulp_v)) {
-                parent.set(ulp_v, ulp_u);
-                size.set(ulp_u, size.get(ulp_u) + size.get(ulp_v));
+        boolean unionBySize(int u, int v) {
+            int parU = findUPar(u), parV = findUPar(v);
+            if(parU == parV) return false;
+            int sizeU = size.get(parU), sizeV = size.get(parV);
+            if(sizeU < sizeV) {
+                parent.set(parU, parV);
+                size.set(parU, sizeU + sizeV);
             } else {
-                parent.set(ulp_u, ulp_v);
-                size.set(ulp_v, size.get(ulp_u) + size.get(ulp_v));
+                parent.set(parV, parU);
+                size.set(parV, sizeU + sizeV);
             }
+            return true;
         }
     }
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-        Map<String, Integer> map = new HashMap<>();
-        int n=accounts.size();
-        DisjointSet ds = new DisjointSet(n);
-        for(int i=0;i<n;i++) {
-            for(int j=1;j<accounts.get(i).size();j++) {
-                String curr = accounts.get(i).get(j);
-                if(map.containsKey(curr)) {
-                    int parent_node = map.get(curr);
-                    ds.unionBySize(i, parent_node);
-                } else {
-                    map.put(curr,i);
-                }
-            }
-        }
-        List<List<String>> tem = new ArrayList<>();
-        for(int i=0;i<n;i++) tem.add(new ArrayList<>());
-        for(Map.Entry<String,Integer> entry:map.entrySet()) {
-            String email = entry.getKey();
-            int node = entry.getValue();
-            int parent_node = ds.findUPar(node);
-            tem.get(parent_node).add(email);
-        }
-        List<List<String>> res = new ArrayList<>();
-        for(int i=0;i<n;i++) {
-            if(tem.get(i).size()>0) {
-                String first = accounts.get(i).get(0);
-                List<String> t = new ArrayList<>();
-                for(String str:tem.get(i)) {
-                    t.add(str);
-                }
-                Collections.sort(t);
-                t.add(0,first);
-                res.add(t);
-            }
-        }
-        return res;
         /**
-            go through each email id and put them into their respective id's in map
-            <emailId, rowNo>
         */
+        int n = accounts.size();
+        DisjointSet ds = new DisjointSet(n);
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<n; i++) {
+            List<String> acc = accounts.get(i);
+            for(int j=1; j<acc.size(); j++) {
+                String email = acc.get(j);
+                if(map.containsKey(email)) {
+                    ds.unionBySize(i, map.get(email));
+                } else {
+                    map.put(email, i);
+                }
+            }
+        }
+
+        List<List<String>> temp = new ArrayList<>();
+        for(int i=0; i<n; i++) temp.add(new ArrayList<>());
+        for(Map.Entry<String, Integer> entry:map.entrySet()) {
+            int par = ds.findUPar(entry.getValue());
+            temp.get(par).add(entry.getKey());
+        }
+        List<List<String>> ans = new ArrayList<>();
+        for(int i=0; i<n; i++) {
+            if(temp.get(i).size()>0) {
+                Collections.sort(temp.get(i));
+                List<String> t = new ArrayList<>();
+                t.add(accounts.get(i).get(0));
+                for(String s:temp.get(i)) t.add(s);
+                ans.add(t);
+            }
+        }
+        return ans;
     }
 }
