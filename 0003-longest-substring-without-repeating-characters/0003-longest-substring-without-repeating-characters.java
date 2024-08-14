6cb8747cc1ddfcfbe0298c49273f414a69dc47ab
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-08-14 14:41:55 +0530

    Time: 6 ms (68.84%), Space: 44.7 MB (31.7%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.java
@@ -1,23 +1,22 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        Queue<Character> q = new LinkedList<>();
-        Set<Character> set = new HashSet<>();
-        int count=0;
-
-        for(int i=0; i<s.length(); i++) {
-            Character ch = s.charAt(i);
-            if(set.contains(ch)) {
-                //queue ke front se keep on polling and deleting the popped element from set and once you reach 'ch' just pop and add it to the front;
-                while(set.contains(ch)) {
-                    Character front = q.poll();
-                    set.remove(front);
-                }
+        /**
+            as the name suggests, give the length of substring with all unique characters
+        */
+        Set<Character> freq = new HashSet<>();
+        char[] arr = s.toCharArray();
+        Set<Character> invalid = new HashSet<>();
+        int l = 0, r = 0, n = s.length(), ans = 0;
+        while(r<n) {
+            char ch = arr[r];
+            while(freq.contains(ch)) {
+                freq.remove(arr[l]);
+                l++;
             }
-            set.add(ch);
-            q.add(ch);
-            count=Math.max(count, set.size());
+            freq.add(ch);
+            ans = Math.max(freq.size(), ans);
+            r++;
         }
-
-        return count;
+        return ans;
     }
 }
