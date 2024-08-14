f4fb5e09117a2822aebbd798e78ecdb7bd5993e0
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-08-14 16:22:12 +0530

    Time: 10 ms (53.26%), Space: 53 MB (74.16%) - LeetHub

--- 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
+++ 1370-count-number-of-nice-subarrays/1370-count-number-of-nice-subarrays.java
@@ -1,20 +1,25 @@
 class Solution {
     public int numberOfSubarrays(int[] nums, int k) {
-        int n = nums.length, next[] = new int[n], last = n;
+        int count=0, l=0, r=0, n = nums.length, ans = 0;
+        int nextOdd[] = new int[n];
+        int lastOdd = n;
         for(int i=n-1; i>=0; i--) {
-            next[i] = last;
-            if((nums[i]&1)==1) last = i;
+            nextOdd[i] = lastOdd;
+            if(nums[i] %2 == 1) lastOdd = i;
         }
-        int l=0, r=0, odd=0, count = 0;
         while(r<n) {
-            odd+=(nums[r]&1);
-            while(odd==k) {
-                count += next[r]-r;
-                odd-=(nums[l]&1);
+            count += nums[r] % 2;
+            while(count > k) {
+                count -= nums[l] % 2;
+                l++;
+            }
+            while(count == k && l<=r) {
+                ans += nextOdd[r] - r;
+                count -= nums[l] % 2;
                 l++;
             }
             r++;
         }
-        return count;
+        return ans;
     }
 }
