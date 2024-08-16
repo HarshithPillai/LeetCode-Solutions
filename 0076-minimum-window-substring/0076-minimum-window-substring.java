76c1faec8dc62199029edc42061357b1faece9de
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-08-16 12:14:23 +0530

    Time: 20 ms (40.8%), Space: 45.5 MB (23.59%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -10,18 +10,20 @@ class Solution {
         Map<Character, Integer> tfreq = new HashMap<>();
         char[] tarr = t.toCharArray();
         for(char ch:tarr) tfreq.put(ch, tfreq.getOrDefault(ch, 0) + 1);
-        int l=0, r = 0, n = s.length(), ansL = -1, ansR = -1, max = (int)1e6;
+        int l=0, r = 0, n = s.length(), ansL = -1, ansR = -1, max = (int)1e7, valid = 0, need = tfreq.size();
         char[] sarr = s.toCharArray();
         while(r<n) {
             char ch = sarr[r];
             sfreq.put(ch, sfreq.getOrDefault(ch, 0) + 1);
-            while(isValid(sfreq, tfreq)) {
+            if(tfreq.containsKey(ch) && sfreq.get(ch).equals(tfreq.get(ch))) valid++;
+            while(valid == need) {
                 if(max > r-l+1) {
                     max = r-l+1;
                     ansL = l;
                     ansR = r;
                 }
                 int val = sfreq.get(sarr[l]);
+                if(tfreq.containsKey(sarr[l]) && val == tfreq.get(sarr[l])) valid--;
                 sfreq.put(sarr[l], val-1);
                 l++;
             }
