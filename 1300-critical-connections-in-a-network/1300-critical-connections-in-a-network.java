4db7f51b6cd2ee4575ac976d64981d1e0ccf5046
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-08-16 20:12:21 +0530

    Time: 69 ms (97%), Space: 126.6 MB (25.14%) - LeetHub

--- 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
+++ 1300-critical-connections-in-a-network/1300-critical-connections-in-a-network.java
@@ -1,37 +1,36 @@
 class Solution {
+    int[] t_in, low;
     int timer = 1;
-    public void dfs(List<List<Integer>> adj, int node, int parent, int[] t_in, int[] low, List<List<Integer>> res) {
-        low[node]=timer;
-        t_in[node]=timer;
+    List<List<Integer>> res = new ArrayList<>();
+    public void dfs(List<List<Integer>> adj, int node, int parent) {
+        t_in[node] = timer;
+        low[node] = timer;
         timer++;
         for(int nbr:adj.get(node)) {
-            if(parent==nbr) continue;
-            if(t_in[nbr]==0) {
-                dfs(adj, nbr, node, t_in, low, res);
-                low[node]=Math.min(low[node],low[nbr]);
-                if(t_in[node] < low[nbr]) {
-                    List<Integer> t=new ArrayList<>();
-                    t.add(node);t.add(nbr);
-                    res.add(t);
+            if(nbr != parent) {
+                if(t_in[nbr] != 0) {
+                    low[node] = Math.min(low[nbr], low[node]);
+                } else {
+                    dfs(adj, nbr, node);
+                    low[node] = Math.min(low[nbr], low[node]);
+                    if(low[nbr] > t_in[node]) {
+                        res.add(List.of(node, nbr));
+                    }
                 }
-            } else {
-                low[node]=Math.min(low[node],low[nbr]);
             }
         }
     }
     public List<List<Integer>> criticalConnections(int n, List<List<Integer>> connections) {
-        int t_in[] = new int[n];
-        int  low[] = new int[n];
-        List<List<Integer>> res = new ArrayList<>();
+        t_in = new int[n];
+        low = new int[n];
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0;i<n;i++) adj.add(new ArrayList<>());
-        for(int i=0;i<connections.size();i++) {
-            int u = connections.get(i).get(0);
-            int v = connections.get(i).get(1);
+        for(int i=0; i<n; i++) adj.add(new ArrayList());
+        for(List<Integer> edge:connections) {
+            int u = edge.get(0), v = edge.get(1);
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-        dfs(adj, 0, -1, t_in, low, res);
+        dfs(adj, 0, -1);
         return res;
     }
 }
