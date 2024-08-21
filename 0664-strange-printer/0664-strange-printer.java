8b944ab258248b4e5489b7cb347406bd6ef8c12e
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-08-21 12:48:26 +0530

    Time: 48 ms (7.76%), Space: 45.3 MB (5.31%) - LeetHub

---
+++ 0664-strange-printer/0664-strange-printer.java
@@ -0,0 +1,20 @@
+class Solution {
+    Integer dp[][] = new Integer[101][101];
+    public int helper(char[] arr, int i, int j) {
+        if(i==j) return 1;
+        if(dp[i][j] != null) return dp[i][j];
+        int res = (int)1e9;
+        for(int k=i; k<j; k++) {
+            int minus = (arr[i] == arr[k+1])?1:0;
+            res = Math.min(res, helper(arr, i, k) + helper(arr, k+1, j) - minus);
+        }
+        return dp[i][j] = res;
+    }
+    public int strangePrinter(String s) {
+        /**
+            Similar to Matric Chain multiplication/ Burst Balloon/ Rod cutting
+        */
+        char[] arr = s.toCharArray();
+        return helper(arr, 0, s.length()-1);
+    }
+}
