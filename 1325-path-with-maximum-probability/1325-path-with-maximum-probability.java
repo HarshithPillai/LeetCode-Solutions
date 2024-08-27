64153edd2a44160d8bbcc87c711ceee88d9c8008
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-08-27 07:07:21 +0530

    Time: 286 ms (5.01%), Space: 57.3 MB (53.85%) - LeetHub

--- 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/1325-path-with-maximum-probability.java
@@ -1,37 +1,39 @@
-// mohit jeet gya - adha kilo jamun
-
 class Solution {
-    public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        double dist[] = new double[n];
-        Queue<double[]> q = new LinkedList<>();
+    public double maxProbability(int n, int[][] edges, double[] succProb, int st, int en) {
+        /**
+            Jamun
+        */
+        
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a,b)->Double.compare(b[0], a[0]));
+
+        pq.add(new double[]{st, 1});
         List<List<double[]>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        for(int i=0; i<n; i++) {
+            adj.add(new ArrayList<>());
+        }
         int ind=0;
-        for(int[] ed:edges) {
-            int u = ed[0];
-            int v = ed[1];
+        for(int[] ed:edges){
+            int u=ed[0], v=ed[1];
             double w = succProb[ind];
-            ind++;
             adj.get(u).add(new double[]{v, w});
             adj.get(v).add(new double[]{u, w});
+            ind++;
         }
-        q.add(new double[]{start_node, 1});
-        double[] ans = new double[n];
-        dist[start_node] = 1;
-        while(!q.isEmpty()) {
-            double[] top = q.poll();
-            int node = (int)top[0];
-            double w = top[1];
-            // if(node == end_node) return w;
+        double[] dist = new double[n];
+        dist[st] = 1;
+        while(!pq.isEmpty()) {
+            double[] top = pq.poll();
+            int node = (int) top[0];
+            double distance = top[1];
             for(double[] next:adj.get(node)) {
-                int nbr = (int)next[0];
-                double weight = next[1];
-                if(w*weight > dist[nbr]) {
-                    dist[nbr] = w*weight;
-                    q.add(new double[]{nbr, dist[nbr]});
+                int nbr = (int) next[0];
+                double w = next[1];
+                if(dist[nbr] < distance*w) {
+                    dist[nbr] = distance*w;
+                    pq.add(new double[]{nbr, dist[nbr]});
                 }
             }
         }
-        return dist[end_node];
+        return dist[en];
     }
 }
