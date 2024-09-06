b14d06c972e477797a7128e938c2b42064e47381
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-09-06 17:49:44 +0530

    Time: 32 ms (43.74%), Space: 54.9 MB (83.47%) - LeetHub

--- 0937-online-stock-span/0937-online-stock-span.java
+++ 0937-online-stock-span/0937-online-stock-span.java
@@ -2,13 +2,11 @@ class StockSpanner {
 
     Stack<Integer> st;
     List<Integer> arr;
-    List<Integer> pge;
     int ind=-1;
 
     public StockSpanner() {
         st = new Stack<>();
         arr = new ArrayList<>();
-        pge = new ArrayList<>();
         ind = -1;
     }
     
@@ -16,7 +14,6 @@ class StockSpanner {
         ind++;
         while(!st.isEmpty() && arr.get(st.peek()) <= price) st.pop();
         arr.add(price);
-        pge.add(ind);
         int ans = 0;
         if(st.isEmpty()) ans = ind+1;
         else ans = ind-st.peek();
