248f19602a5f2949bebce4c524c30c67bf17cbe1
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-09-07 18:11:43 +0530

    Time: 2 ms (89.66%), Space: 56.3 MB (16.85%) - LeetHub

--- 0890-lemonade-change/0890-lemonade-change.java
+++ 0890-lemonade-change/0890-lemonade-change.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        int count5 = 0, count10 = 0;
+        int count5=0, count10=0;
         for(int bill:bills) {
-            if(bill == 5) count5++;
-            else if(bill == 10) {
-                if(count5 == 0) return false;
+            if(bill == 5) {
+                count5++;
+            } else if(bill == 10) {
+                if(count5==0) return false;
                 count5--;
                 count10++;
             } else {
-                if((count5==0 || count10 == 0) && (count5 < 3)) return false;
-                if(count5>0 && count10>0) {
-                    count5--;
-                    count10--;
-                } else {
+                if(count5==0) return false;
+                if(count10==0 && count5 < 3) return false;
+                if(count10 == 0) {
                     count5-=3;
+                } else {
+                    count10--;
+                    count5--;
                 }
             }
         }
