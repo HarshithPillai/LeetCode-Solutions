3058972fded846562b255b63ea56d7a4cffc9ed0
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-09-07 18:29:27 +0530

    Time: 28 ms (6.83%), Space: 57.5 MB (80.72%) - LeetHub

---
+++ 2497-maximum-matching-of-players-with-trainers/2497-maximum-matching-of-players-with-trainers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int matchPlayersAndTrainers(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int indg = g.length-1, inds = s.length-1;
+        int ans = 0;
+        while(indg>=0 && inds>=0) {
+            while(indg>=0 && g[indg] > s[inds]) indg--;
+            if(indg == -1) break;
+            int greed = g[indg--];
+            if(greed <= s[inds]) {
+                ans++;
+                inds--;
+            }
+        }
+        return ans;
+    }
+}
