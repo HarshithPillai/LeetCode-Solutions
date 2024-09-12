e9d9f91b583986cee879e528e2598974cf0cd962
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-09-12 12:55:48 +0530

    Time: 8 ms (69.67%), Space: 44.3 MB (55.55%) - LeetHub

--- 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
+++ 0719-find-k-th-smallest-pair-distance/0719-find-k-th-smallest-pair-distance.java
@@ -1,39 +1,46 @@
 class Solution {
+    int n;
     public int helper(int[] nums, int mid) {
-        int l=0, r = 1;
-        int count = 0;
-        while(r<nums.length) {
+        /**
+            two pointer question: find pairs of number with difference less than or equal to mid
+            
+            
+            ans += length of window - 1 (becuase here this means
+                                    that all the pairs ending at 'r'
+                                    are all having distance <= mid)           
+        */
+
+        int count = 0, l = 0, r = 1;
+        while(r<n) {
             while(nums[r]-nums[l] > mid) {
                 l++;
             }
-            count+=r-l;
+            count += r-l;
             r++;
         }
         return count;
     }
     public int smallestDistancePair(int[] nums, int k) {
-        /** 
-            helper function:
-                check the pairs and calculate the total pairs that have a diff <= mid
-            
-            binary search in the search space of answers
+        /**
+            smallest distance among all the pairs of nums[i] and nums[j]
+            sort the array
+            binary search on the search space of possible pairs
+            and find the one which one gives the count of k pairs 
+            with difference equal to or lesser than mid
         */
-        int l=0, h=(int)1e6;
         Arrays.sort(nums);
+        n = nums.length;
+        int l = 0, h = nums[n-1]-nums[0];
         while(l<=h) {
-            int mid = l+(h-l)/2;
+            int mid = l + (h-l)/2;
             int count = helper(nums, mid);
             if(count >= k) {
-                // total number of pairs with diff <= mid is more than or equal to k
-                // this could be a possible answer
-                // we need to minimize mid
-                // how do we minimize mid?
-                // go left
                 h = mid-1;
             } else {
                 l = mid+1;
             }
         }
+
         return l;
     }
 }
