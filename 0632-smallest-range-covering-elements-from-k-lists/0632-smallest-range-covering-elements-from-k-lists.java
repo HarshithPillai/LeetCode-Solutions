317045579def644231c46470a5f46cc47cbb59f8
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-10-13 13:14:38 +0530

    Time: 34 ms (50.87%), Space: 48.8 MB (46.34%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.java
@@ -1,28 +1,25 @@
 class Solution {
     public int[] smallestRange(List<List<Integer>> nums) {
-        int min = (int)1e6, max = -(int)1e6, k = nums.size();
-        int minList = -1, maxList = -1;
-        int[] indices = new int[k], ans = new int[]{max, min};
+        int max = -(int)1e6, k = nums.size(), range = (int)1e6, start = -1, end = -1;
+        PriorityQueue<int[]> pq = new PriorityQueue<>((a,b)->Integer.compare(a[0], b[0]));
+        for(int i=0; i<k; i++) {
+            int val = nums.get(i).get(0);
+            pq.add(new int[]{val, i, 0});
+            max = Math.max(max, val);
+        }
         // boolean flag = true;
         while(true) {
-            min = (int)1e6;
-            max = -(int)1e6;
-            for(int i=0; i<k; i++) {
-                int ind = indices[i];
-                int val = nums.get(i).get(ind);
-                if(val > max) {
-                    max = val;
-                }
-                if (val < min) {
-                    min = val;
-                    minList = i;
-                }
+            int[] index = pq.poll();
+            int val = index[0], listInd = index[1], ind = index[2];
+            if(max - val < range) {
+                start = val;
+                end = max;
+                range = max - val;
             }
-            // System.out.println(min + " " + max);
-            if(ans[1] - ans[0] > max - min) ans = new int[]{min, max};
-            indices[minList]++;
-            if(indices[minList] == nums.get(minList).size()) break;
+            if(ind + 1 == nums.get(listInd).size()) break;
+            pq.add(new int[]{nums.get(listInd).get(ind+1), listInd, ind+1});
+            max = Math.max(max, nums.get(listInd).get(ind+1));
         }
-        return ans;
+        return new int[]{start, end};
     }
 }
