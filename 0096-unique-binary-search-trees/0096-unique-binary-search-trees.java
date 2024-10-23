4e31a45b859a9766b721065b2ec5d0319f2a9179
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-10-23 13:41:34 +0530

    Time: 0 ms (100%), Space: 40.6 MB (18.08%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,14 +1,17 @@
 class Solution {
+    Integer[][] dp;
     public int dfs(int s, int e) {
         if(e < s) return 0;
         if(e-s < 2) return e-s+1;
+        if(dp[s][e] != null) return dp[s][e];
         int curr = dfs(s, e-1) + dfs(s+1, e);
         for(int i=s+1; i<e; i++) {
             curr += dfs(s, i-1) * dfs(i+1, e);
         }
-        return curr;
+        return dp[s][e] = curr;
     }
     public int numTrees(int n) {
+        dp = new Integer[n+1][n+1];
         return dfs(1, n);
     }
 }
