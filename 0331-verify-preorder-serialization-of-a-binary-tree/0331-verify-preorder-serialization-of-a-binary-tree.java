f294e2bc0edccdc71360ce1c7b883c4c439c21f8
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-10-23 15:16:21 +0530

    Time: 4 ms (90.05%), Space: 43.5 MB (42.54%) - LeetHub

--- 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
+++ 0331-verify-preorder-serialization-of-a-binary-tree/0331-verify-preorder-serialization-of-a-binary-tree.java
@@ -2,7 +2,6 @@ class Solution {
     public boolean isValidSerialization(String preorder) {
         String[] arr = preorder.split(",");
         int curr = 1;
-        if(arr[0].equals("#")) return arr.length == 1;
         for(String val:arr) {
             if(curr<=0) return false;
             if(!val.equals("#")) curr += 2;
