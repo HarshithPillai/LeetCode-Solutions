372268cfa32f31edc15cf5e605044d978b2707ac
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-10-23 13:51:35 +0530

    Time: 0 ms (100%), Space: 39.8 MB (95.55%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.java
@@ -1,17 +1,31 @@
 class Solution {
-    Integer[][] dp;
-    public int dfs(int s, int e) {
-        if(e < s) return 0;
-        if(e-s < 2) return e-s+1;
-        if(dp[s][e] != null) return dp[s][e];
-        int curr = dfs(s, e-1) + dfs(s+1, e);
-        for(int i=s+1; i<e; i++) {
-            curr += dfs(s, i-1) * dfs(i+1, e);
-        }
-        return dp[s][e] = curr;
-    }
+    int[][] dp;
+    // public int dfs(int s, int e) {
+    //     if(e < s) return 0;
+    //     if(e-s < 2) return e-s+1;
+    //     if(dp[s][e] != null) return dp[s][e];
+    //     int curr = dfs(s, e-1) + dfs(s+1, e);
+    //     for(int i=s+1; i<e; i++) {
+    //         curr += dfs(s, i-1) * dfs(i+1, e);
+    //     }
+    //     return dp[s][e] = curr;
+    // }
     public int numTrees(int n) {
-        dp = new Integer[n+1][n+1];
-        return dfs(1, n);
+        dp = new int[n+1][n+1];
+        for(int i=1; i<=n; i++) {
+            dp[i][i] = 1;
+            if(i<n) dp[i][i+1] = 2;
+        }
+        for(int s = n; s>=1; s--) {
+            for(int e = 1; e<=n; e++) {
+                if(e-s < 2) continue;
+                int curr = ((e>1)?dp[s][e-1]:0) + ((s<n)?dp[s+1][e]:0);
+                for(int i=s+1; i<e; i++) {
+                    curr += dp[s][i-1] * dp[i+1][e];
+                }
+                dp[s][e] = curr;
+            }
+        }
+        return dp[1][n];
     }
 }
