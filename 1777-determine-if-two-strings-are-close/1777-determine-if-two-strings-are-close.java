c8a5eed7b744cd171263d62a59a3ef855c5d7bdb
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-10-24 14:45:09 +0530

    Time: 32 ms (52.73%), Space: 45.2 MB (90.62%) - LeetHub

--- 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/1777-determine-if-two-strings-are-close.java
@@ -1,26 +1,21 @@
 class Solution {
     public boolean closeStrings(String word1, String word2) {
         char[] a = word1.toCharArray(), b = word2.toCharArray();
-        Map<Character, Integer> map1 = new HashMap<>(), map2 = new HashMap<>();
-        for(char ch:a) map1.put(ch, map1.getOrDefault(ch, 0) + 1);
-        for(char ch:b) map2.put(ch, map2.getOrDefault(ch, 0) + 1);
-        List<Character> l1 = new ArrayList<>(map1.keySet()), l2 = new ArrayList<>(map2.keySet());
-        if(l1.size() != l2.size()) return false;
-        Collections.sort(l1);
-        Collections.sort(l2);
-        for(int i=0; i<l1.size(); i++) {
-            if(!l1.get(i).equals(l2.get(i))) {
-                return false;
-            }
+        int[] freq1 = new int[26], freq2 = new int[26];
+        Set<Character> s = new HashSet<>();
+        for(char ch:a) {
+            freq1[ch-'a']++;
+            s.add(ch);
         }
-        List<Integer> l1i = new ArrayList<>(map1.values()), l2i = new ArrayList<>(map2.values());
-        if(l1i.size() != l2i.size()) return false;
-        Collections.sort(l1i);
-        Collections.sort(l2i);
-        for(int i=0; i<l1i.size(); i++) {
-            if(!l1i.get(i).equals(l2i.get(i))) {
-                return false;
-            }
+        for(char ch:b) {
+            if(!s.contains(ch)) return false;
+            freq2[ch-'a']++;
+        }
+        Arrays.sort(freq1);
+        Arrays.sort(freq2);
+
+        for(int i=0; i<26; i++) {
+            if(freq1[i] != freq2[i]) return false;
         }
 
         return true;
