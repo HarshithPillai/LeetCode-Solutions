4540b1bb77f1f3ec87e3c391462967157375c613
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-11-01 22:07:55 +0530

    Time: 6 ms (88.66%), Space: 44.7 MB (21.71%) - LeetHub

--- 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
+++ 0803-cheapest-flights-within-k-stops/0803-cheapest-flights-within-k-stops.java
@@ -1,18 +1,35 @@
 class Solution {
     public int findCheapestPrice(int n, int[][] flights, int src, int dst, int k) {
-        int cost[] = new int[n];
-        Arrays.fill(cost, (int)1e9);
-        cost[src] = 0;
-        for(int i=0; i<=k; i++) {
-            int temp[] = new int[n];
-            for(int ia=0; ia<n; ia++) temp[ia] = cost[ia];
-            for(int ed[]:flights) {
-                int u = ed[0], v = ed[1], w = ed[2];
-                temp[v] = Math.min(temp[v], cost[u] + w);
-            }
-            cost = temp;
+        int[] dist = new int[n];
+        Arrays.fill(dist, (int)1e9);
+        dist[src] = 0;
+        List<List<int[]>> adj = new ArrayList<>();
+        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
+        for(int edge[]:flights) {
+            int u = edge[0], v = edge[1], w = edge[2];
+            adj.get(u).add(new int[]{v,w});
         }
-        if(cost[dst] == (int)1e9) return -1;
-        return cost[dst];
+        k++;
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{src, 0});
+        while(!q.isEmpty()) {
+            if(k==0) break;
+            int size = q.size();
+            for(int i=1; i<=size; i++) {
+                int top[] = q.poll();
+                int node = top[0];
+                int wt = top[1];
+                // if(node == dst) return wt;
+                for(int[] next:adj.get(node)) {
+                    if(dist[next[0]] > wt+next[1] ) {
+                        dist[next[0]] = wt+next[1];
+                        q.add(new int[]{next[0], wt+next[1]});
+                    }
+                }
+            }
+            k--;
+        }
+        if(dist[dst] == (int)1e9) return -1;
+        return dist[dst];
     }
 }
