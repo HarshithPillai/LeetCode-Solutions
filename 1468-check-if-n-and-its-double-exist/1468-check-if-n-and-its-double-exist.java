83c57780308e4cffddb3f3480e7996447fcd1a0c
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-12-01 11:19:31 +0530

    Time: 2 ms (93.52%), Space: 43.5 MB (14.48%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -2,7 +2,7 @@ class Solution {
     public boolean checkIfExist(int[] arr) {
         Set<Integer> set = new HashSet<>();
         int countZero = 0;
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
         for(int i=arr.length-1; i>=0; i--) {
             if(arr[i]==0) countZero++;
             if(set.contains(arr[i]*2) || ((arr[i]%2 == 0) && set.contains(arr[i]/2))) return true;
@@ -12,7 +12,7 @@ class Solution {
         //     set.add(arr[i]);
         //     if(set.contains(arr[i]*2) && arr[i]!=0) return true;
         // }
-        if(countZero > 1) return true;
+        // if(countZero > 1) return true;
         return false;
     }
 }
