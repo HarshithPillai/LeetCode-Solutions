1b23bd59a030ee14201f28f60d81ac05565ff5af
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2024-12-01 11:19:12 +0530

    Time: 5 ms (8.99%), Space: 42.8 MB (52.15%) - LeetHub

--- 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
+++ 1468-check-if-n-and-its-double-exist/1468-check-if-n-and-its-double-exist.java
@@ -5,13 +5,13 @@ class Solution {
         Arrays.sort(arr);
         for(int i=arr.length-1; i>=0; i--) {
             if(arr[i]==0) countZero++;
+            if(set.contains(arr[i]*2) || ((arr[i]%2 == 0) && set.contains(arr[i]/2))) return true;
             set.add(arr[i]);
-            if(set.contains(arr[i]*2) && arr[i]!=0) return true;
         }
-        for(int i=arr.length-1; i>=0; i--) {
-            set.add(arr[i]);
-            if(set.contains(arr[i]*2) && arr[i]!=0) return true;
-        }
+        // for(int i=arr.length-1; i>=0; i--) {
+        //     set.add(arr[i]);
+        //     if(set.contains(arr[i]*2) && arr[i]!=0) return true;
+        // }
         if(countZero > 1) return true;
         return false;
     }
