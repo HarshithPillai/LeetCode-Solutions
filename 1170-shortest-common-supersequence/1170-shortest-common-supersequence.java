c51fadec22bbf6269c14d517f83b3b045cf2bd6d
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-02-28 13:23:24 +0530

    Time: 8 ms (99.69%), Space: 44.4 MB (100%) - LeetHub

--- 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
+++ 1170-shortest-common-supersequence/1170-shortest-common-supersequence.java
@@ -1,48 +1,41 @@
 class Solution {
-    int n, m;
     public String shortestCommonSupersequence(String str1, String str2) {
-        char[] arr1 = str1.toCharArray(), arr2 = str2.toCharArray();
-        n = arr1.length;
-        m = arr2.length;
-        int[][] dp = new int[n+1][m+1];
-        for (int i=1; i<=n; i++) {
-            for (int j=1; j<=m; j++) {
-                if (arr1[i-1] == arr2[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
+        StringBuilder sb = new StringBuilder();
+        char[] a=str1.toCharArray(),b=str2.toCharArray();
+        int n1=a.length, n2=b.length;
+        int[][] dp = new int[n1+1][n2+1];
+        for(int i=1;i<=n1;i++) {
+            for(int j=1;j<=n2;j++) {
+                if(a[i-1]==b[j-1]) {
+                    dp[i][j]=1+dp[i-1][j-1];
                 } else {
-                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    dp[i][j]=Math.max(dp[i-1][j],dp[i][j-1]);
                 }
             }
         }
-        
-        int i = n, j = m;
-        StringBuilder sb = new StringBuilder();
-        while (i>0 && j>0) {
-            while (i > 0 && dp[i][j] == dp[i-1][j]) {
-                sb.append(arr1[i-1]);
-                i--;
+        int x=n1, y=n2;
+        while(x>0 && y>0) {
+            if(a[x-1]==b[y-1]) {
+                sb.append(a[x-1]);
+                x--;y--;
+            } else {
+                if(dp[x][y-1]>dp[x-1][y]) {
+                    sb.append(b[y-1]);
+                    y--;
+                } else {
+                    sb.append(a[x-1]);
+                    x--;
+                }
             }
-            while (j > 0 && dp[i][j] == dp[i][j-1]) {
-                sb.append(arr2[j-1]);
-                j--;
-            }
-            if(i > 0) sb.append(arr1[i-1]);
-            i--;
-            j--;
         }
-
-        while (i > 0) {
-            sb.append(arr1[i-1]);
-            i--;
+        while(x>0) {
+            sb.append(a[x-1]);
+            x--;
         }
-
-        while (j > 0) {
-            sb.append(arr2[j-1]);
-            j--;
+        while(y>0) {
+            sb.append(b[y-1]);
+            y--;
         }
-
-        sb.reverse();
-
-        return new String(sb);
+        return sb.reverse().toString();
     }
 }
