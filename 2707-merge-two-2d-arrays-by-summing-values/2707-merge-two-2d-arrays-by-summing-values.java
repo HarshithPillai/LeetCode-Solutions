777c66674ab5ffac61d8690d968659609f0c7a07
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-02 22:53:04 +0530

    Time: 1 ms (86.98%), Space: 45.4 MB (47.9%) - LeetHub

--- 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
+++ 2707-merge-two-2d-arrays-by-summing-values/2707-merge-two-2d-arrays-by-summing-values.java
@@ -3,14 +3,12 @@ class Solution {
         List<int[]> lt = new ArrayList<>();
         int ind = -1, n = nums1.length, m = nums2.length, i = 0, j = 0;
         while (i < n && j < m) {
-            if (nums1[i][0] != nums2[j][0]) {
-                if (nums1[i][0] < nums2[j][0]) {
-                    lt.add(nums1[i]);
-                    i++;
-                } else {
-                    lt.add(nums2[j]);
-                    j++;
-                }
+            if (nums1[i][0] < nums2[j][0]) {
+                lt.add(nums1[i]);
+                i++;
+            } else if (nums1[i][0] > nums2[j][0]) {
+                lt.add(nums2[j]);
+                j++;
             } else {
                 nums1[i][1] += nums2[j][1];
                 j++;
