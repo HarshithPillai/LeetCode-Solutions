7437d8e864ebc145448bee9a4a482564315b6a45
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-14 22:53:08 +0530

    Time: 38 ms (21.24%), Space: 63.9 MB (7.93%) - LeetHub

--- 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
+++ 1335-maximum-candies-allocated-to-k-children/1335-maximum-candies-allocated-to-k-children.java
@@ -3,13 +3,6 @@ class Solution {
     int n;
     public boolean helper(int[] candies, long mid, long k) {
         for (int i = n-1; i >= 0 && k > 0; i--) {
-            // if (curr >= mid) {
-            //     k--;
-            //     curr -= mid;
-            // } else {
-            //     if (i > 0) curr = candies[i-1];
-            //     i--;
-            // }
             if (candies[i] >= mid) {
                 k -= candies[i]/mid;
             }
@@ -28,10 +21,10 @@ class Solution {
             and some piles can go unused - no need to use all the piles
         */
         
-        Arrays.sort(candies);
+        // Arrays.sort(candies);
         n = candies.length;
-        long l = 1, r = candies[n-1];
-        // for (int pile : candies) r = Math.max(r, pile);
+        long l = 1, r = candies[0];
+        for (int pile : candies) r = Math.max(r, pile);
         
         while (l <= r) {
             long mid = l + (r - l) / 2;
