6529ec4c7fe2a512cfe7ed31b63837531901418e
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-18 01:18:34 +0530

    Time: 11 ms (70.94%), Space: 58.1 MB (47.14%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,20 +1,9 @@
 class Solution {
-    // from accepted pool
     public boolean containsDuplicate(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (num < min || num > max) {
-                min = Math.min(num, min);
-                max = Math.max(num, max);
-                continue;
-            }
-            for (int j = 0; j < i; j++) {
-                if (nums[i] == nums[j]) {
-                    return true;
-                }
-            }
+        Set<Integer> set = new HashSet<>();
+        for (int num : nums) {
+            if (set.contains(num)) return true;
+            set.add(num);
         }
         return false;
     }
