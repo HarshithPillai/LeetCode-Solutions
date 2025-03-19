822daa8b3bb158d879c7c264b987c7218004f1af
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-19 11:15:27 +0530

    Time: 7 ms (66.53%), Space: 57.9 MB (9.04%) - LeetHub

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -4,10 +4,10 @@ class Solution {
             3 consecutive elements from the array get flipped
             all must by set bit '1'
         */
-        int ans = 0;
-        for (int i = 0; i < nums.length; i++) {
+        int ans = 0, n = nums.length;
+        for (int i = 0; i < n; i++) {
             if (nums[i] == 0) {
-                if (i >= nums.length-2) return -1;
+                if (i >= n-2) return -1;
                 for (int j = 0; j < 3; j++) nums[i + j] ^= 1;
                 ans++;
             }
