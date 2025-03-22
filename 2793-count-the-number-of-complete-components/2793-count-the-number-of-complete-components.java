36ed04dc7791e54b0551e747867918b9153e30a0
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-22 11:00:23 +0530

    Time: 11 ms (57.88%), Space: 45.2 MB (72.09%) - LeetHub

--- 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
+++ 2793-count-the-number-of-complete-components/2793-count-the-number-of-complete-components.java
@@ -1,39 +1,38 @@
 class Solution {
-    int ans = 0, curr = 0;
-    Set<Integer> set;
-    int[] vis;
-    boolean valid = true;
-    public void dfs(List<List<Integer>> adj, int node) {
-        vis[node] = 1;
-        for(int nbr:adj.get(node)) {
-            if(vis[nbr] == 0) {
-                if(!set.contains(node)) valid = false;
-                dfs(adj, nbr);
+    public void helper(List<List<Integer>> adj, int[] vis, int node, List<Integer> comp) {
+        comp.add(node);
+        for (int nbr : adj.get(node)) {
+            if (vis[nbr] == 0) {
+                vis[nbr] = 1;
+                helper(adj, vis, nbr, comp);
             }
         }
-        if(adj.get(node).size() != curr || !set.contains(node)) valid = false;
     }
     public int countCompleteComponents(int n, int[][] edges) {
         List<List<Integer>> adj = new ArrayList<>();
-        for(int i=0; i<n; i++) adj.add(new ArrayList<>());
-        for(int[] edge:edges) {
-            int u = edge[0], v = edge[1];
+        for (int i = 0; i < n; i++) adj.add(new ArrayList<>());
+        for (int[] ed : edges) {
+            int u = ed[0], v = ed[1];
             adj.get(u).add(v);
             adj.get(v).add(u);
         }
-        vis = new int[n];
-        for(int i=0; i<n; i++) {
-            // for(int nbr:adj.get(i)) {
-            //     System.out.print(nbr + " ");
-            // }
-            // System.out.println("");
-            if(vis[i] == 0) {
-                curr = adj.get(i).size();
-                set = new HashSet<>(adj.get(i));
-                set.add(i);
-                dfs(adj, i);
-                if(valid) ans++;
-                valid = true;
+
+        int[] vis = new int[n];
+        int ans = 0;
+        for (int i = 0; i < n; i++) {
+            if (vis[i] == 0) {
+                List<Integer> component = new ArrayList<>();
+                boolean flag = true;
+                vis[i] = 1;
+                helper(adj, vis, i, component);
+                int size = component.size();
+                for (int node : component) {
+                    if (adj.get(node).size() != size - 1) {
+                        flag = false;
+                        break;
+                    }
+                }
+                if (flag) ans++;
             }
         }
         return ans;
