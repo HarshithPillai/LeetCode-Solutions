32f1839115e2829e095e74b0303145b9e56ae3c8
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-24 17:59:21 +0530

    Time: 53 ms (22.56%), Space: 100.3 MB (66.67%) - LeetHub

---
+++ 3430-count-days-without-meetings/3430-count-days-without-meetings.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int countDays(int days, int[][] meetings) {
+        /**
+            integer days representing the total number of days an employee is available for work
+            from day 1
+            2D array meetings of size n
+            where, meetings[i] = [start, end]
+            return count of days when employee is available for work but no meetings are scheduled
+        */
+
+        Arrays.sort(meetings, (a, b) -> {
+            if (a[0] == b[0]) return Integer.compare(b[1], a[1]);
+            return Integer.compare(a[0], b[0]);
+        });
+
+        int last = 0, ans = 0;
+        for (int[] meet : meetings) {
+            int s = meet[0], e = meet[1];
+            // System.out.println(s + " : " + e);
+            if (s > last) {
+                ans += s - last - 1;
+            }
+            last = Math.max(last, e);
+        }
+
+        if (days > last) ans += days - last;
+
+        return ans;
+    }
+}
