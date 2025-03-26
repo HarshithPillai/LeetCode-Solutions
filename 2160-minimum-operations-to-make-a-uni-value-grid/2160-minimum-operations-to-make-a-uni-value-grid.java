804a385af72dd9ede5adae9026cb1bb4c94a7a5c
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-26 11:54:16 +0530

    Time: 29 ms (95.48%), Space: 66.3 MB (91.96%) - LeetHub

---
+++ 2160-minimum-operations-to-make-a-uni-value-grid/2160-minimum-operations-to-make-a-uni-value-grid.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int minOperations(int[][] grid, int x) {
+        /**
+            m * n grid, and an integer x
+            add x or subtract x to/from any element in the grid
+            return minimum number of operations required to make all the elements equal
+            if it's not possible, return -1
+        */
+        int n = grid.length, m = grid[0].length, ind = 0, sum = 0, mod = grid[0][0] % x;
+        int[] arr = new int[n * m], prefix = new int[n * m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (grid[i][j] % x != mod) return -1;
+                arr[ind++] = grid[i][j];
+            }
+        }
+
+        Arrays.sort(arr);
+        n = n * m;
+        
+        for (int i = 0; i < n; i++) {
+            sum += arr[i];
+            prefix[i] = sum;
+        }
+
+        int min = (int) 1e9;
+        for (int i = 0; i < n; i++) {
+            int leftreq = (i > 0) ? i * arr[i] : 0, op = 0;
+            if (leftreq > 0) op = (leftreq - prefix[i - 1]) / x;
+            int rightreq = (i < n - 1) ? (n - i - 1) * arr[i] : 0;
+            if (rightreq > 0) op += (prefix[n-1] - prefix[i] - rightreq) / x;
+            min = Math.min(min, op);
+        }
+        return min;
+    }
+}
