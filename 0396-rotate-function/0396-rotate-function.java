d2d3cfe55bef9b55f16bea904144ecb7c55ab9b6
Harshith Pillai <42529664+HarshithPillai@users.noreply.github.com>
2025-03-27 12:44:49 +0530

    Time: 4 ms (82.56%), Space: 59.8 MB (25.97%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.java
+++ 0396-rotate-function/0396-rotate-function.java
@@ -2,6 +2,23 @@ class Solution {
     public int maxRotateFunction(int[] nums) {
         /**
             use dp to get the optimal position for '0'
+            https://www.youtube.com/watch?v=Die0sok3WTM
+            
+            sum = sum of all elements
+
+            f[0] = 0 * nums[1] + 1 * nums[2] + .... + (n-2) * nums[n-1] + (n-1) * nums[0]
+            f[1] = 0 * nums[0] + 1 * nums[1] + .... + (n-1) * nums[n-1]
+            f[1] - f[0] = nums[1] + nums[2] + nums[3] + ... + nums[n-1] - (n-1) * nums[0]
+            f[1] - f[0] = sum - n * nums[n-1]
+            f[1] = f[0] + sum - n * nums[n-1]
+            f[2] = f[1] + sum - n * nums[n-2]
+            f[3] = f[2] + sum - n * nums[n-3]
+            .
+            .
+            .
+            f[n-1] = f[n-2] + sum - n * nums[0];
+
+            return the max of all the f(x)
         */
 
         int f = 0, sum = 0, n = nums.length;
